c4be4239aa8813e238eb76f3efc208fa72f0af0a
allada <thegreatall@gmail.com>
2022-06-16 18:51:57 -0500

    Add Send trait to as_any() store calls
    This will be used in future PR that need to pass this across threads.

--- cas/store/dedup_store.rs
+++ cas/store/dedup_store.rs
@@ -325,7 +325,7 @@ impl StoreTrait for DedupStore {
         Ok(())
     }
 
-    fn as_any(self: Arc<Self>) -> Box<dyn std::any::Any> {
+    fn as_any(self: Arc<Self>) -> Box<dyn std::any::Any + Send> {
         Box::new(self)
     }
 }
