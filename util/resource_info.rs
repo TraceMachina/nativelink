b010b4bd019e3e4cce5e5115b0ff797c45e85d96
allada <thegreatall@gmail.com>
2022-10-16 20:43:03 -0500

    Fix bug if no instance_name/resource_name is given upload does not work
    Fixes a bug that if an upload is attempted but no instance_name is
    configured it will error out.

--- util/resource_info.rs
+++ util/resource_info.rs
@@ -24,26 +24,36 @@ pub struct ResourceInfo<'a> {
 
 impl<'a> ResourceInfo<'a> {
     pub fn new(resource_name: &'a str) -> Result<ResourceInfo<'a>, Error> {
-        let mut parts = resource_name.splitn(6, '/');
+        let mut parts = resource_name.splitn(6, '/').peekable();
         const ERROR_MSG: &str = concat!(
             "Expected resource_name to be of pattern ",
-            "'{instance_name}/uploads/{uuid}/blobs/{hash}/{size}' or ",
-            "'{instance_name}/blobs/{hash}/{size}'",
+            "'{?instance_name/}uploads/{uuid}/blobs/{hash}/{size}' or ",
+            "'{?instance_name/}blobs/{hash}/{size}'",
         );
-        let instance_name = &parts.next().err_tip(|| ERROR_MSG)?;
-        let mut blobs_or_uploads: &str = parts.next().err_tip(|| ERROR_MSG)?;
+
+        let mut instance_name = "";
+        let maybe_instance_name = parts.next().err_tip(|| ERROR_MSG)?;
+        let peek_next = parts.peek().err_tip(|| ERROR_MSG)?;
+        let blobs_or_uploads = if *peek_next == "uploads" || *peek_next == "blobs" {
+            instance_name = maybe_instance_name;
+            // We do have an instance_name, so set blobs_or_uploads to the peek_next and consume it.
+            parts.next().err_tip(|| ERROR_MSG)?
+        } else {
+            // We don't have an instance_name, so use our first item as blobs_or_uploads.
+            maybe_instance_name
+        };
         let mut uuid = None;
-        if &blobs_or_uploads == &"uploads" {
+        if blobs_or_uploads == "uploads" {
             uuid = Some(parts.next().err_tip(|| ERROR_MSG)?);
-            blobs_or_uploads = parts.next().err_tip(|| ERROR_MSG)?;
+            let blobs = parts.next().err_tip(|| ERROR_MSG)?;
+            error_if!(
+                blobs != "blobs",
+                "Expected resource_name to have 'blobs' here. Got: {}",
+                blobs_or_uploads
+            );
         }
 
-        error_if!(
-            &blobs_or_uploads != &"blobs",
-            "Element 2 or 4 of resource_name should have been 'blobs'. Got: {}",
-            blobs_or_uploads
-        );
-        let hash = &parts.next().err_tip(|| ERROR_MSG)?;
+        let hash = parts.next().err_tip(|| ERROR_MSG)?;
         let raw_digest_size = parts.next().err_tip(|| ERROR_MSG)?;
         let expected_size = raw_digest_size.parse::<usize>().err_tip(|| {
             format!(
@@ -51,8 +61,9 @@ impl<'a> ResourceInfo<'a> {
                 raw_digest_size
             )
         })?;
+
         Ok(ResourceInfo {
-            instance_name: instance_name,
+            instance_name,
             uuid,
             hash,
             expected_size,
