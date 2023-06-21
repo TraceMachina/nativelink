c823ab49600f86ae290ba9953f9f140d62e16aca
Chris Staite <christopher.staite@menlosecurity.com>
2023-06-21 10:56:07 +0000

    Add Docker example for Goma build.

---
+++ deployment-examples/docker-compose-goma/worker_precondition_script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+
+AVAILABLE_MEMORY_KB=$(awk '/MemAvailable/ { printf "%d \n", $2 }' /proc/meminfo);
+# At least 2Gb of RAM currently available
+if [ $AVAILABLE_MEMORY_KB -gt 2000000 ]; then
+    exit 0
+else
+    echo "Available memory: ${AVAILABLE_MEMORY_KB}";
+    exit 1
+fi
