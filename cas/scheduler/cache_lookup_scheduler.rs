b14932ed80384960d661ca65b2d18a3ac11404a4
Chris Staite <christopher.staite@menlosecurity.com>
2023-07-07 11:36:31 +0000

    Implementation of a cache lookup scheduler.
    This scheduler sits in front of another one and looks up actions in
    the cache if requested before queueing them.

---
+++ cas/scheduler/cache_lookup_scheduler.rs
@@ -0,0 +1,159 @@
+// Copyright 2023 The Turbo Cache Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::collections::HashMap;
+use std::pin::Pin;
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use futures::stream::{FuturesUnordered, StreamExt};
+use rand::{thread_rng, Rng};
+use tokio::sync::watch;
+use tonic::Request;
+
+use ac_utils::get_and_decode_digest;
+use action_messages::{ActionInfo, ActionStage, ActionState};
+use common::DigestInfo;
+use config::schedulers::CacheLookupScheduler as SchedulerConfig;
+use error::{make_input_err, Error};
+use grpc_store::GrpcStore;
+use platform_property_manager::PlatformPropertyManager;
+use proto::build::bazel::remote::execution::v2::{
+    ActionResult as ProtoActionResult, FindMissingBlobsRequest, GetActionResultRequest,
+};
+use scheduler::ActionScheduler;
+use store::{Store, StoreManager};
+
+pub struct CacheLookupScheduler {
+    /// A reference to the CAS which is used to validate all the outputs of a
+    /// cached ActionResult still exist.
+    cas_store: Arc<dyn Store>,
+    /// A reference to the AC to find existing actions in.
+    ac_store: Arc<dyn Store>,
+    /// The "real" scheduler to use to perform actions if they were not found
+    /// in the action cache.
+    scheduler: Arc<dyn ActionScheduler>,
+}
+
+impl CacheLookupScheduler {
+    pub fn new(
+        config: &SchedulerConfig,
+        store_manager: &StoreManager,
+        scheduler_manager: &HashMap<String, Arc<dyn ActionScheduler>>,
+    ) -> Result<Self, Error> {
+        let cas_store = store_manager
+            .get_store(&config.cas_store)
+            .ok_or_else(|| make_input_err!("'cas_store': '{}' does not exist", config.cas_store))?;
+        let ac_store = store_manager
+            .get_store(&config.ac_store)
+            .ok_or_else(|| make_input_err!("'ac_store': '{}' does not exist", config.ac_store))?;
+        let scheduler = scheduler_manager
+            .get(&config.scheduler)
+            .ok_or_else(|| make_input_err!("'scheduler': '{}' does not exist", config.scheduler))?
+            .clone();
+        Ok(Self {
+            cas_store,
+            ac_store,
+            scheduler,
+        })
+    }
+
+    async fn get_action_from_store(&self, action_digest: &DigestInfo) -> Option<ProtoActionResult> {
+        // If we are a GrpcStore we shortcut here, as this is a special store.
+        let any_store = self.ac_store.clone().as_any();
+        let maybe_grpc_store = any_store.downcast_ref::<Arc<GrpcStore>>();
+        let result = if let Some(grpc_store) = maybe_grpc_store {
+            let action_result_request = GetActionResultRequest {
+                instance_name: "".to_string(),
+                action_digest: Some(action_digest.into()),
+                inline_stdout: false,
+                inline_stderr: false,
+                inline_output_files: Vec::new(),
+            };
+            grpc_store
+                .get_action_result(Request::new(action_result_request))
+                .await
+                .map(|response| response.into_inner())
+        } else {
+            get_and_decode_digest::<ProtoActionResult>(Pin::new(self.ac_store.as_ref()), &action_digest).await
+        };
+        result.ok()
+    }
+
+    async fn validate_outputs_exist(&self, action_result: &ProtoActionResult) -> bool {
+        // Verify that output_files and output_directories are available in the cas.
+        let required_digests = action_result
+            .output_files
+            .iter()
+            .filter_map(|output_file| output_file.digest.clone())
+            .chain(
+                action_result
+                    .output_directories
+                    .iter()
+                    .filter_map(|output_directory| output_directory.tree_digest.clone()),
+            )
+            .collect();
+
+        // If the CAS is a GrpcStore store we can check all the digests in one message.
+        let any_store = self.cas_store.clone().as_any();
+        let maybe_grpc_store = any_store.downcast_ref::<Arc<GrpcStore>>();
+        if let Some(grpc_store) = maybe_grpc_store {
+            grpc_store
+                .find_missing_blobs(Request::new(FindMissingBlobsRequest {
+                    instance_name: "".to_string(),
+                    blob_digests: required_digests,
+                }))
+                .await
+                .is_ok_and(|find_result| find_result.into_inner().missing_blob_digests.is_empty())
+        } else {
+            let cas_pin = Pin::new(self.cas_store.as_ref());
+            let mut check_futures: FuturesUnordered<_> = required_digests
+                .iter()
+                .map(|digest| async move { cas_pin.has(DigestInfo::try_from(digest.clone())?).await })
+                .collect();
+            while let Some(result) = check_futures.next().await {
+                if result.is_err() {
+                    return false;
+                }
+            }
+            true
+        }
+    }
+}
+
+#[async_trait]
+impl ActionScheduler for CacheLookupScheduler {
+    async fn get_platform_property_manager(&self, instance_name: &str) -> Result<Arc<PlatformPropertyManager>, Error> {
+        self.scheduler.get_platform_property_manager(&instance_name).await
+    }
+
+    async fn add_action(&self, action_info: ActionInfo) -> Result<watch::Receiver<Arc<ActionState>>, Error> {
+        if !action_info.skip_cache_lookup {
+            let action_digest = action_info.digest();
+            if let Some(proto_action_result) = self.get_action_from_store(action_digest).await {
+                if self.validate_outputs_exist(&proto_action_result).await {
+                    // Found in the cache, return the result immediately.
+                    let current_state = Arc::new(ActionState {
+                        name: format!("{:X}", thread_rng().gen::<u128>()),
+                        stage: ActionStage::CompletedFromCache(proto_action_result),
+                        action_digest: action_digest.clone(),
+                    });
+                    return Ok(watch::channel(current_state).1);
+                }
+            }
+        }
+        // Not in cache or invalid, forward to real scheduler.
+        self.scheduler.add_action(action_info).await
+    }
+}
--- cas/scheduler/default_scheduler_factory.rs
+++ cas/scheduler/default_scheduler_factory.rs
@@ -12,21 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::pin::Pin;
 use std::sync::Arc;
 
 use futures::Future;
 
+use cache_lookup_scheduler::CacheLookupScheduler;
 use config::schedulers::SchedulerConfig;
 use error::Error;
 use grpc_scheduler::GrpcScheduler;
 use scheduler::{ActionScheduler, WorkerScheduler};
 use simple_scheduler::SimpleScheduler;
+use store::StoreManager;
 
 pub type SchedulerFactoryResults = (Option<Arc<dyn ActionScheduler>>, Option<Arc<dyn WorkerScheduler>>);
 
 pub fn scheduler_factory<'a>(
     scheduler_type_cfg: &'a SchedulerConfig,
+    store_manager: &'a StoreManager,
+    scheduler_manager: &'a HashMap<String, Arc<dyn ActionScheduler>>,
 ) -> Pin<Box<dyn Future<Output = Result<SchedulerFactoryResults, Error>> + 'a>> {
     Box::pin(async move {
         let scheduler: SchedulerFactoryResults = match scheduler_type_cfg {
@@ -35,6 +40,14 @@ pub fn scheduler_factory<'a>(
                 (Some(scheduler.clone()), Some(scheduler))
             }
             SchedulerConfig::grpc(config) => (Some(Arc::new(GrpcScheduler::new(&config).await?)), None),
+            SchedulerConfig::cache_lookup(config) => (
+                Some(Arc::new(CacheLookupScheduler::new(
+                    &config,
+                    &store_manager,
+                    &scheduler_manager,
+                )?)),
+                None,
+            ),
         };
         Ok(scheduler)
     })
