76326dbf0d8c92b9d233f00ffe3fcef9632049c2
allada <thegreatall@gmail.com>
2023-07-15 22:01:45 -0500

    Add TSAN suppression and harness
    Bazel does not make it easy to add a file to every test as a
    dependency. But it does have --run_under, so we hijack this
    in order to make our suppression file. Later we may want to
    make a macro override for our test rules to add the suppression
    file if it grows too large.

---
+++ tools/tsan.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2022 The Turbo Cache Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file is used to run tests under ThreadSanitizer. Generally it should
+# be invoked under bazel using `bazel run --config tsan //{target_here}`.
+set -euo pipefail
+
+export TMPDIR="${TEST_TMPDIR:-${TMPDIR:-/tmp}}"
+tsan_suppresions_file=$(mktemp -t tsan_suppressions.XXXXXX)
+trap "rm -f $tsan_suppresions_file" EXIT
+
+cat <<EOF > "$tsan_suppresions_file"
+race:std::rt::lang_start_internal
+EOF
+
+export TSAN_OPTIONS="suppressions=$tsan_suppresions_file"
+export RUST_TEST_THREADS=1
+# Note: We cannot use `exec` here or else our `trap` cleanup will not run.
+"$@"
