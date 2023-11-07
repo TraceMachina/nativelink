504c1e9190edff5e60908d208b6c74616cee5b70
Blake Hatch <blakewihatch@gmail.com>
2023-11-07 12:30:02 -0500

    Added existence cache

--- cas/store/tests/verify_store_test.rs
+++ cas/store/tests/verify_store_test.rs
@@ -220,3 +220,271 @@ mod verify_store_tests {
         Ok(())
     }
 }
+// Copyright 2022 The Turbo Cache Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::pin::Pin;
+use std::sync::Arc;
+
+use futures::try_join;
+
+#[cfg(test)]
+mod verify_store_tests {
+    use super::*;
+    use pretty_assertions::assert_eq; // Must be declared in every module.
+
+    use buf_channel::make_buf_channel_pair;
+    use common::DigestInfo;
+    use error::{Error, ResultExt};
+    use memory_store::MemoryStore;
+    use traits::{StoreTrait, UploadSizeInfo};
+    use verify_store::VerifyStore;
+
+    const VALID_HASH1: &str = "0123456789abcdef000000000000000000010000000000000123456789abcdef";
+
+    #[tokio::test]
+    async fn verify_size_false_passes_on_update() -> Result<(), Error> {
+        let inner_store = Arc::new(MemoryStore::new(&config::stores::MemoryStore::default()));
+        let store_owned = VerifyStore::new(
+            &config::stores::VerifyStore {
+                backend: config::stores::StoreConfig::memory(config::stores::MemoryStore::default()),
+                verify_size: false,
+                verify_hash: false,
+            },
+            inner_store.clone(),
+        );
+        let store = Pin::new(&store_owned);
+
+        const VALUE1: &str = "123";
+        let digest = DigestInfo::try_new(VALID_HASH1, 100).unwrap();
+        let result = store.update_oneshot(digest, VALUE1.into()).await;
+        assert_eq!(
+            result,
+            Ok(()),
+            "Should have succeeded when verify_size = false, got: {:?}",
+            result
+        );
+        assert_eq!(
+            Pin::new(inner_store.as_ref()).has(digest).await,
+            Ok(Some(VALUE1.len())),
+            "Expected data to exist in store after update"
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn verify_size_true_fails_on_update() -> Result<(), Error> {
+        let inner_store = Arc::new(MemoryStore::new(&config::stores::MemoryStore::default()));
+        let store_owned = VerifyStore::new(
+            &config::stores::VerifyStore {
+                backend: config::stores::StoreConfig::memory(config::stores::MemoryStore::default()),
+                verify_size: true,
+                verify_hash: false,
+            },
+            inner_store.clone(),
+        );
+        let store = Pin::new(&store_owned);
+
+        const VALUE1: &str = "123";
+        let digest = DigestInfo::try_new(VALID_HASH1, 100).unwrap();
+        let (mut tx, rx) = make_buf_channel_pair();
+        let send_fut = async move {
+            tx.send(VALUE1.into()).await?;
+            tx.send_eof().await
+        };
+        let result = try_join!(send_fut, store.update(digest, rx, UploadSizeInfo::ExactSize(100)));
+        assert!(result.is_err(), "Expected error, got: {:?}", &result);
+        const EXPECTED_ERR: &str = "Expected size 100 but got size 3 on insert";
+        let err = result.unwrap_err().to_string();
+        assert!(
+            err.contains(EXPECTED_ERR),
+            "Error should contain '{}', got: {:?}",
+            EXPECTED_ERR,
+            err
+        );
+        assert_eq!(
+            Pin::new(inner_store.as_ref()).has(digest).await,
+            Ok(None),
+            "Expected data to not exist in store after update"
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn verify_size_true_suceeds_on_update() -> Result<(), Error> {
+        let inner_store = Arc::new(MemoryStore::new(&config::stores::MemoryStore::default()));
+        let store_owned = VerifyStore::new(
+            &config::stores::VerifyStore {
+                backend: config::stores::StoreConfig::memory(config::stores::MemoryStore::default()),
+                verify_size: true,
+                verify_hash: false,
+            },
+            inner_store.clone(),
+        );
+        let store = Pin::new(&store_owned);
+
+        const VALUE1: &str = "123";
+        let digest = DigestInfo::try_new(VALID_HASH1, 3).unwrap();
+        let result = store.update_oneshot(digest, VALUE1.into()).await;
+        assert_eq!(result, Ok(()), "Expected success, got: {:?}", result);
+        assert_eq!(
+            Pin::new(inner_store.as_ref()).has(digest).await,
+            Ok(Some(VALUE1.len())),
+            "Expected data to exist in store after update"
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn verify_size_true_suceeds_on_multi_chunk_stream_update() -> Result<(), Error> {
+        let inner_store = Arc::new(MemoryStore::new(&config::stores::MemoryStore::default()));
+        let store_owned = VerifyStore::new(
+            &config::stores::VerifyStore {
+                backend: config::stores::StoreConfig::memory(config::stores::MemoryStore::default()),
+                verify_size: true,
+                verify_hash: false,
+            },
+            inner_store.clone(),
+        );
+
+        let (mut tx, rx) = make_buf_channel_pair();
+
+        let digest = DigestInfo::try_new(VALID_HASH1, 6).unwrap();
+        let digest_clone = digest;
+        let future = tokio::spawn(async move {
+            Pin::new(&store_owned)
+                .update(digest_clone, rx, UploadSizeInfo::ExactSize(6))
+                .await
+        });
+        tx.send("foo".into()).await?;
+        tx.send("bar".into()).await?;
+        tx.send_eof().await?;
+        let result = future.await.err_tip(|| "Failed to join spawn future")?;
+        assert_eq!(result, Ok(()), "Expected success, got: {:?}", result);
+        assert_eq!(
+            Pin::new(inner_store.as_ref()).has(digest).await,
+            Ok(Some(6)),
+            "Expected data to exist in store after update"
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn verify_hash_true_suceeds_on_update() -> Result<(), Error> {
+        let inner_store = Arc::new(MemoryStore::new(&config::stores::MemoryStore::default()));
+        let store_owned = VerifyStore::new(
+            &config::stores::VerifyStore {
+                backend: config::stores::StoreConfig::memory(config::stores::MemoryStore::default()),
+                verify_size: false,
+                verify_hash: true,
+            },
+            inner_store.clone(),
+        );
+        let store = Pin::new(&store_owned);
+
+        /// This value is sha256("123").
+        const HASH: &str = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3";
+        const VALUE: &str = "123";
+        let digest = DigestInfo::try_new(HASH, 3).unwrap();
+        let result = store.update_oneshot(digest, VALUE.into()).await;
+        assert_eq!(result, Ok(()), "Expected success, got: {:?}", result);
+        assert_eq!(
+            Pin::new(inner_store.as_ref()).has(digest).await,
+            Ok(Some(VALUE.len())),
+            "Expected data to exist in store after update"
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn verify_hash_true_fails_on_update() -> Result<(), Error> {
+        let inner_store = Arc::new(MemoryStore::new(&config::stores::MemoryStore::default()));
+        let store_owned = VerifyStore::new(
+            &config::stores::VerifyStore {
+                backend: config::stores::StoreConfig::memory(config::stores::MemoryStore::default()),
+                verify_size: false,
+                verify_hash: true,
+            },
+            inner_store.clone(),
+        );
+        let store = Pin::new(&store_owned);
+
+        /// This value is sha256("12").
+        const HASH: &str = "6b51d431df5d7f141cbececcf79edf3dd861c3b4069f0b11661a3eefacbba918";
+        const VALUE: &str = "123";
+        let digest = DigestInfo::try_new(HASH, 3).unwrap();
+        let result = store.update_oneshot(digest, VALUE.into()).await;
+        let err = result.unwrap_err().to_string();
+        const ACTUAL_HASH: &str = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3";
+        let expected_err = format!("Hashes do not match, got: {} but digest hash was {}", HASH, ACTUAL_HASH);
+        assert!(
+            err.contains(&expected_err),
+            "Error should contain '{}', got: {:?}",
+            expected_err,
+            err
+        );
+        assert_eq!(
+            Pin::new(inner_store.as_ref()).has(digest).await,
+            Ok(None),
+            "Expected data to not exist in store after update"
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_existence_cache() -> Result<(), Error> {
+        let inner_store = Arc::new(MemoryStore::new(&config::stores::MemoryStore::default()));
+        let store_owned = VerifyStore::new(
+            &config::stores::VerifyStore {
+                backend: config::stores::StoreConfig::memory(config::stores::MemoryStore::default()),
+                verify_size: false,
+                verify_hash: false,
+            },
+            inner_store.clone(),
+        );
+        let store = Pin::new(&store_owned);
+
+        let digest = DigestInfo::try_new(VALID_HASH1, 3).unwrap();
+
+        let _result = store.has(digest).await;
+        assert!(
+            store.has_in_cache(&digest).await,
+            "Expected digest to exist in cache after has call"
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_walk_and_record_existence_cache() -> Result<(), Error> {
+        let inner_store = Arc::new(MemoryStore::new(&config::stores::MemoryStore::default()));
+        let store_owned = VerifyStore::new(
+            &config::stores::VerifyStore {
+                backend: config::stores::StoreConfig::memory(config::stores::MemoryStore::default()),
+                verify_size: false,
+                verify_hash: false,
+            },
+            inner_store.clone(),
+        );
+        let store = Arc::new(store_owned);
+
+        let digest = DigestInfo::try_new(VALID_HASH1, 3).unwrap();
+
+        let _result = Arc::clone(&store).record_existence_directory_walk(digest).await;
+        assert!(
+            store.has_in_cache(&digest).await,
+            "Expected digest to exist in cache after walking tree"
+        );
+        Ok(())
+    }
+}
