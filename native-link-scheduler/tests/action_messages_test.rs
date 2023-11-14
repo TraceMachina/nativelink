48f8190cc573e9af6aa7f3ae868c53b976daaf95
Aaron Siddhartha Mondal <aaron@eomii.org>
2023-11-14 22:40:00 +0100

    Rewrite the build infrastructure
    Our previous build layout was fairly custom and hard to maintain.
    Migrate to a more standards-conform layout that is compatible with good
    Cargo and Bazel practices and feels natural to develop in.
    
    This commit swaps the previous "one-crate-per-file" layout to a layout
    where we have a few medium sized crates that are just fine-grained
    enough to allow almost the same amount of parallelization as the
    previous fine-grained layout.
    
    The new layout enables dependency auto-updaters, standard cargo tooling
    and doc autogeneration. This should improve DX quite a bit and gives us
    some much needed compatibility with nix/cargo interop and should make
    most standard cargo extensions compatible with the repository.
    
    Some notable changes to the file layout:
    
    - The previous `error` crate is no longer part of `utils`. It is now a
      standalone module. This was necessary to keep macro exports compatible
      with the Cargo build.
    - The old `scheduler.rs` file is now split into `action_scheduler.rs`
      and `worker_scheduler.rs` since we don't want imports like
      `native_link_scheduler::scheduler`.
    - Some tests now use a slightly different import mechanism for test-only
      dependencies.
    - The store traits are now in `native_link_utils`, as it was used
      virtually everywhere. Moving this up in the dependency graph allows
      for higher parallelization of the other crates.
    
    Added documentation targets for every crate (except the `cas` binary
    itself, as this will require a dedicated commit). These doc targets can
    be viewed by pointing a browser to
    `bazel-bin/thetarget.rustdoc/thetarget/index.html`.
    
    Added rustfmt configuration to standardize imports. We now use `crate::`
    syntax which plays well with rusfmts's import formatting. The new layout
    makes it easier to spot crate/directory interdependencies so the we can
    remove those in future commits.

---
+++ native-link-scheduler/tests/action_messages_test.rs
@@ -0,0 +1,208 @@
+// Copyright 2023 The Native Link Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::collections::{BTreeSet, HashMap};
+use std::sync::Arc;
+use std::time::{Duration, SystemTime};
+
+use error::Error;
+use native_link_scheduler::action_messages::{
+    ActionInfo, ActionInfoHashKey, ActionResult, ActionStage, ActionState, ExecutionMetadata,
+};
+use native_link_scheduler::platform_property_manager::PlatformProperties;
+use native_link_util::common::DigestInfo;
+use native_link_util::digest_hasher::DigestHasherFunc;
+use proto::build::bazel::remote::execution::v2::ExecuteResponse;
+use proto::google::longrunning::{operation, Operation};
+use proto::google::rpc::Status;
+
+const NOW_TIME: u64 = 10000;
+
+fn make_system_time(add_time: u64) -> SystemTime {
+    SystemTime::UNIX_EPOCH
+        .checked_add(Duration::from_secs(NOW_TIME + add_time))
+        .unwrap()
+}
+
+#[cfg(test)]
+mod action_messages_tests {
+    use pretty_assertions::assert_eq;
+
+    use super::*; // Must be declared in every module.
+
+    #[tokio::test]
+    async fn action_state_any_url_test() -> Result<(), Error> {
+        let action_state = ActionState {
+            unique_qualifier: ActionInfoHashKey {
+                instance_name: "foo_instance".to_string(),
+                digest: DigestInfo::new([1u8; 32], 5),
+                salt: 0,
+            },
+            // Result is only populated if has_action_result.
+            stage: ActionStage::Completed(ActionResult::default()),
+        };
+        let operation: Operation = action_state.clone().into();
+
+        match &operation.result {
+            Some(operation::Result::Response(any)) => assert_eq!(
+                any.type_url,
+                "type.googleapis.com/build.bazel.remote.execution.v2.ExecuteResponse"
+            ),
+            other => panic!("Expected Some(Result(Any)), got: {other:?}"),
+        }
+
+        let action_state_round_trip: ActionState = operation.try_into()?;
+        assert_eq!(action_state, action_state_round_trip);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn execute_response_status_message_is_some_on_success_test() -> Result<(), Error> {
+        let execute_response: ExecuteResponse = ActionStage::Completed(ActionResult {
+            output_files: vec![],
+            output_folders: vec![],
+            output_file_symlinks: vec![],
+            output_directory_symlinks: vec![],
+            exit_code: 0,
+            stdout_digest: DigestInfo::new([2u8; 32], 5),
+            stderr_digest: DigestInfo::new([3u8; 32], 5),
+            execution_metadata: ExecutionMetadata {
+                worker: "foo_worker_id".to_string(),
+                queued_timestamp: SystemTime::UNIX_EPOCH,
+                worker_start_timestamp: SystemTime::UNIX_EPOCH,
+                worker_completed_timestamp: SystemTime::UNIX_EPOCH,
+                input_fetch_start_timestamp: SystemTime::UNIX_EPOCH,
+                input_fetch_completed_timestamp: SystemTime::UNIX_EPOCH,
+                execution_start_timestamp: SystemTime::UNIX_EPOCH,
+                execution_completed_timestamp: SystemTime::UNIX_EPOCH,
+                output_upload_start_timestamp: SystemTime::UNIX_EPOCH,
+                output_upload_completed_timestamp: SystemTime::UNIX_EPOCH,
+            },
+            server_logs: HashMap::default(),
+            error: None,
+            message: String::new(),
+        })
+        .into();
+
+        // This was once discovered to be None, which is why this test exists.
+        assert_eq!(execute_response.status, Some(Status::default()));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn highest_priority_action_first() -> Result<(), Error> {
+        const INSTANCE_NAME: &str = "foobar_instance_name";
+
+        let high_priority_action = Arc::new(ActionInfo {
+            command_digest: DigestInfo::new([0u8; 32], 0),
+            input_root_digest: DigestInfo::new([0u8; 32], 0),
+            timeout: Duration::MAX,
+            platform_properties: PlatformProperties {
+                properties: HashMap::new(),
+            },
+            priority: 1000,
+            load_timestamp: SystemTime::UNIX_EPOCH,
+            insert_timestamp: SystemTime::UNIX_EPOCH,
+            unique_qualifier: ActionInfoHashKey {
+                instance_name: INSTANCE_NAME.to_string(),
+                digest: DigestInfo::new([0u8; 32], 0),
+                salt: 0,
+            },
+            skip_cache_lookup: true,
+            digest_function: DigestHasherFunc::Sha256,
+        });
+        let lowest_priority_action = Arc::new(ActionInfo {
+            command_digest: DigestInfo::new([0u8; 32], 0),
+            input_root_digest: DigestInfo::new([0u8; 32], 0),
+            timeout: Duration::MAX,
+            platform_properties: PlatformProperties {
+                properties: HashMap::new(),
+            },
+            priority: 0,
+            load_timestamp: SystemTime::UNIX_EPOCH,
+            insert_timestamp: SystemTime::UNIX_EPOCH,
+            unique_qualifier: ActionInfoHashKey {
+                instance_name: INSTANCE_NAME.to_string(),
+                digest: DigestInfo::new([1u8; 32], 0),
+                salt: 0,
+            },
+            skip_cache_lookup: true,
+            digest_function: DigestHasherFunc::Sha256,
+        });
+        let mut action_set = BTreeSet::<Arc<ActionInfo>>::new();
+        action_set.insert(lowest_priority_action.clone());
+        action_set.insert(high_priority_action.clone());
+
+        assert_eq!(
+            vec![high_priority_action, lowest_priority_action],
+            action_set.iter().rev().cloned().collect::<Vec<Arc<ActionInfo>>>()
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn equal_priority_earliest_first() -> Result<(), Error> {
+        const INSTANCE_NAME: &str = "foobar_instance_name";
+
+        let first_action = Arc::new(ActionInfo {
+            command_digest: DigestInfo::new([0u8; 32], 0),
+            input_root_digest: DigestInfo::new([0u8; 32], 0),
+            timeout: Duration::MAX,
+            platform_properties: PlatformProperties {
+                properties: HashMap::new(),
+            },
+            priority: 0,
+            load_timestamp: SystemTime::UNIX_EPOCH,
+            insert_timestamp: SystemTime::UNIX_EPOCH,
+            unique_qualifier: ActionInfoHashKey {
+                instance_name: INSTANCE_NAME.to_string(),
+                digest: DigestInfo::new([0u8; 32], 0),
+                salt: 0,
+            },
+            skip_cache_lookup: true,
+            digest_function: DigestHasherFunc::Sha256,
+        });
+        let current_action = Arc::new(ActionInfo {
+            command_digest: DigestInfo::new([0u8; 32], 0),
+            input_root_digest: DigestInfo::new([0u8; 32], 0),
+            timeout: Duration::MAX,
+            platform_properties: PlatformProperties {
+                properties: HashMap::new(),
+            },
+            priority: 0,
+            load_timestamp: SystemTime::UNIX_EPOCH,
+            insert_timestamp: make_system_time(0),
+            unique_qualifier: ActionInfoHashKey {
+                instance_name: INSTANCE_NAME.to_string(),
+                digest: DigestInfo::new([1u8; 32], 0),
+                salt: 0,
+            },
+            skip_cache_lookup: true,
+            digest_function: DigestHasherFunc::Sha256,
+        });
+        let mut action_set = BTreeSet::<Arc<ActionInfo>>::new();
+        action_set.insert(current_action.clone());
+        action_set.insert(first_action.clone());
+
+        assert_eq!(
+            vec![first_action, current_action],
+            action_set.iter().rev().cloned().collect::<Vec<Arc<ActionInfo>>>()
+        );
+
+        Ok(())
+    }
+}
---
+++ native-link-service/src/lib.rs
@@ -0,0 +1,20 @@
+// Copyright 2023 The Native Link Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub mod ac_server;
+pub mod bytestream_server;
+pub mod capabilities_server;
+pub mod cas_server;
+pub mod execution_server;
+pub mod worker_api_server;
