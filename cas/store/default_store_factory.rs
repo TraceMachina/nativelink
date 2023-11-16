03370182dfdbad4b429d52f70dc7ae12de22903e
Blake Hatch <blakewihatch@gmail.com>
2023-11-16 12:19:47 -0500

    Add Completeness Checking Store

--- cas/store/default_store_factory.rs
+++ cas/store/default_store_factory.rs
@@ -18,10 +18,12 @@ use std::sync::Arc;
 use futures::stream::FuturesOrdered;
 use futures::{Future, TryStreamExt};
 
+use completeness_checking_store::CompletenessCheckingStore;
 use compression_store::CompressionStore;
 use config::{self, stores::StoreConfig};
 use dedup_store::DedupStore;
 use error::Error;
+use existence_store::ExistenceStore;
 use fast_slow_store::FastSlowStore;
 use filesystem_store::FilesystemStore;
 use grpc_store::GrpcStore;
@@ -45,6 +47,10 @@ pub fn store_factory<'a>(
         let store: Arc<dyn Store> = match backend {
             StoreConfig::memory(config) => Arc::new(MemoryStore::new(config)),
             StoreConfig::s3_store(config) => Arc::new(S3Store::new(config)?),
+            StoreConfig::completeness_checking_store(config) => Arc::new(CompletenessCheckingStore::new(
+                store_factory(&config.backend, store_manager, None).await?,
+                store_factory(&config.backend, store_manager, None).await?,
+            )),
             StoreConfig::verify(config) => Arc::new(VerifyStore::new(
                 config,
                 store_factory(&config.backend, store_manager, None).await?,
@@ -57,6 +63,9 @@ pub fn store_factory<'a>(
                 config,
                 store_factory(&config.index_store, store_manager, None).await?,
                 store_factory(&config.content_store, store_manager, None).await?,
+            )),
+            StoreConfig::existence_store(config) => Arc::new(ExistenceStore::new(
+                store_factory(&config.inner, store_manager, None).await?,
             )),
             StoreConfig::fast_slow(config) => Arc::new(FastSlowStore::new(
                 config,
