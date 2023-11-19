f68ac06d9c0a16126f9911a6ee3420cd88654d28
Aaron Siddhartha Mondal <aaron@eomii.org>
2023-11-19 01:21:31 +0100

    Add default pre-commit hooks (#405)
    These have mostly no effect and just make sure that trailing newlines
    are used properly, linebreaks are linux-like and trailing whitespace is
    removed.

--- deployment-examples/terraform/AWS/scripts/create_filesystem.sh
+++ deployment-examples/terraform/AWS/scripts/create_filesystem.sh
@@ -19,7 +19,7 @@
 set -euxo pipefail
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
--- tools/pre-commit-hooks.nix
+++ tools/pre-commit-hooks.nix
@@ -1,4 +1,55 @@
 { pkgs, ... }:
+let
+
+excludes = ["^gencargo/" "^proto/genproto"];
+
+in
 {
-  # TODO(aaronmondal): Add hooks here.
+  # Default hooks
+  trailing-whitespace-fixer = {
+    inherit excludes;
+    enable = true;
+    name = "trailing-whitespace";
+    description = "Remove trailing whitespace";
+    entry = "${pkgs.python311Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
+    types = [ "text" ];
+  };
+  end-of-file-fixer = {
+    inherit excludes;
+    enable = true;
+    name = "end-of-file-fixer";
+    description = "Remove trailing whitespace";
+    entry = "${pkgs.python311Packages.pre-commit-hooks}/bin/end-of-file-fixer";
+    types = [ "text" ];
+  };
+  fix-byte-order-marker = {
+    inherit excludes;
+    enable = true;
+    name = "fix-byte-order-marker";
+    entry = "${pkgs.python311Packages.pre-commit-hooks}/bin/fix-byte-order-marker";
+  };
+  mixed-line-ending = {
+    inherit excludes;
+    enable = true;
+    name = "mixed-line-ending";
+    entry = "${pkgs.python311Packages.pre-commit-hooks}/bin/mixed-line-ending";
+    types = [ "text" ];
+  };
+  check-case-conflict = {
+    inherit excludes;
+    enable = true;
+    name = "check-case-conflict";
+    entry = "${pkgs.python311Packages.pre-commit-hooks}/bin/check-case-conflict";
+    types = [ "text" ];
+  };
+  detect-private-key = {
+    excludes = excludes ++ [
+      # This is an integration testfile that is not intended for production.
+      "deployment-examples/docker-compose/example-do-not-use-in-prod-key.pem"
+    ];
+    enable = true;
+    name = "detect-private-key";
+    entry = "${pkgs.python311Packages.pre-commit-hooks}/bin/detect-private-key";
+    types = [ "text" ];
+  };
 }
