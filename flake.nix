0f7a2289e2a334e1da3d1b8d595f2b828e1b1a3f
Aaron Siddhartha Mondal <aaron@eomii.org>
2023-12-01 22:19:10 +0100

    Make Native Link installable via nix
    This commit makes Native Link directly buildable/runnable via nix:
    
    ```bash
    nix run github:TraceMachina/native-link /path/to/config.json
    ```
    
    To reduce closure size for upcoming container images the environment
    that builds the derivation is a fairly customized LLVM-based toolchain.
    This toolchain should make it easier to migrate to more sophisticated
    approaches in the future.
    
    Test infrastructure is not yet enabled as it'll be a larger effort to
    make our testsuite compatible with strictly controlled environments like
    the nix build sandbox.
    
    Importantly, the updated README now makes extensive use of emojis ✨

--- flake.nix
+++ flake.nix
@@ -3,18 +3,68 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs @ { flake-parts, crane, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
       imports = [ inputs.pre-commit-hooks.flakeModule ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
+          customStdenv = import ./tools/llvmStdenv.nix { inherit pkgs; };
+
+          craneLib = crane.lib.${system};
+
+          src = pkgs.lib.cleanSourceWith {
+            src = craneLib.path ./.;
+            filter = path: type:
+              (builtins.match "^.+/data/SekienAkashita\\.jpg" path != null) ||
+              (craneLib.filterCargoSources path type);
+          };
+
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
+            buildInputs = [ ];
+            nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.cacert ];
+            stdenv = customStdenv;
+          };
+
+          # Additional target for external dependencies to simplify caching.
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          native-link = craneLib.buildPackage (commonArgs
+            // {
+            inherit cargoArtifacts;
+          });
+
           hooks = import ./tools/pre-commit-hooks.nix { inherit pkgs; };
         in
         {
+          apps = {
+            default = {
+              type = "app";
+              program = "${native-link}/bin/cas";
+            };
+          };
+          checks = {
+            # TODO(aaronmondal): Fix the tests.
+            # tests = craneLib.cargoNextest (commonArgs
+            #   // {
+            #   inherit cargoArtifacts;
+            #   cargoNextestExtraArgs = "--all";
+            #   partitions = 1;
+            #   partitionType = "count";
+            # });
+          };
           pre-commit.settings = { inherit hooks; };
           devShells.default = pkgs.mkShell {
             nativeBuildInputs = [
