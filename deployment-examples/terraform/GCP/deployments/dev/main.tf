c015c06ac48b78f127e9b9b3d072df7ffdc00e57
Nathan (Blaise) Bruer <github.blaise@allada.com>
2023-11-30 21:42:55 -0600

    Add GCP to terraform deployment examples
    Adds Google Cloud deploymnet example to the "deployment-examples"
    terraform directory.

---
+++ deployment-examples/terraform/GCP/deployments/dev/main.tf
@@ -0,0 +1,42 @@
+# Copyright 2023 The Native Link Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.5.0"
+    }
+  }
+  # backend "gcs" {
+  #   bucket = "nldev-terraform-state-bucket-blaise-dev-404022"
+  #   prefix = "terraform/dev/state"
+  # }
+  required_version = ">= 1.6.3"
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+}
+
+module "native_link" {
+  source = "../../module"
+
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = var.gcp_region
+  gcp_zone       = var.gcp_zone
+  project_prefix = var.project_prefix
+}
