2b3813c4d4ae360080a428cf6f6e57d2081db819
Aaron Siddhartha Mondal <aaron@eomii.org>
2023-12-06 16:53:59 +0100

    Add Kubernetes example
    This example starts a fairly complete Kubernetes cluster and showcases
    perfectly reproducible remote execution via the local remote execution
    toolchain containers.
    
    This example uses a three-layer setup process:
    
    1. The infra layer is a kind cluster with Cilium and MetalLB. This layer
       is built to be easily swappable with more "production grade"
       clusters.
    2. The operations layer deploys a few standard applications that are not
       inherently required for NativeLink, but are solid deployments that
       one would likely want running in a cluster. This includes monitoring
       and handling image availability.
    3. The application layer is a straightforward `kubectl apply -k .` which
       deploys a NativeLink CAS, Worker and Scheduler. This deployment
       differs from the Docker Compose setup in that it does not make use of
       any system paths and doesn't allow visibility "outside" of the node
       itself. That is, it's a hard requirement that the worker image is
       self-contained. Storage is fully ephemeral in this example and a
       `kubectl delete -k .` will destroy the cache for quick iterations and
       cache testing.

---
+++ deployment-examples/kubernetes/03_delete_application.sh
@@ -0,0 +1,11 @@
+# Get the nix derivation hash from the toolchain container, change the
+# `TOOLCHAIN_TAG` variable in the `worker.json.template` to that hash and delete
+# the configuration.
+
+KUSTOMIZE_DIR=$(git rev-parse --show-toplevel)/deployment-examples/kubernetes
+
+sed "s/__NATIVELINK_TOOLCHAIN_TAG__/$(nix eval .#lre.imageTag --raw)/g" \
+  "$KUSTOMIZE_DIR/worker.json.template" \
+  > "$KUSTOMIZE_DIR/worker.json"
+
+kubectl delete -k "$KUSTOMIZE_DIR"
