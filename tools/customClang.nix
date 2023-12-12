22b5cd86c14d04a3b431189befa0eaf960d8377d
Aaron Siddhartha Mondal <aaron@eomii.org>
2023-12-12 18:27:26 +0100

    Introduce Local Remote Execution
    Provide generated toolchain configurations and a reproducible remote
    execution toolchain container with a C++ toolchain that can be used for
    fully hermetic integration tests.
    
    Once it's stabilized this allows us to distribute application-specific
    toolchain containers and to share CI caches with contributors to
    eliminate redundant rebuilds.

---
+++ tools/customClang.nix
@@ -0,0 +1,22 @@
+{ pkgs, stdenv }:
+
+# Bazel expects a single frontend for both C and C++. That works for GCC but
+# not for clang. This wrapper selects `clang` or `clang++` depending on file
+# ending.
+# TODO(aaronmondal): The necessity of this is a bug.
+#                    See: https://github.com/NixOS/nixpkgs/issues/216047
+#                    and https://github.com/NixOS/nixpkgs/issues/150655
+pkgs.writeShellScriptBin "customClang" ''
+  #! ${stdenv.shell}
+  function isCxx() {
+     if [ $# -eq 0 ]; then false
+     elif [ "$1" == '-xc++' ]; then true
+     elif [[ -f "$1" && "$1" =~ [.](hh|H|hp|hxx|hpp|HPP|h[+]{2}|tcc|cc|cp|cxx|cpp|CPP|c[+]{2}|C)$ ]]; then true
+     else isCxx "''${@:2}"; fi
+  }
+  if isCxx "$@"; then
+    exec "${stdenv.cc}/bin/clang++" "$@"
+  else
+    exec "${stdenv.cc}/bin/clang" "$@"
+  fi
+''
