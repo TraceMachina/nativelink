1ed81bb3e673758938d3af1e64596f2dedb46586
Aaron Siddhartha Mondal <aaron@eomii.org>
2023-12-12 18:27:26 +0100

    Introduce Local Remote Execution
    Provide generated toolchain configurations and a reproducible remote
    execution toolchain container with a C++ toolchain that can be used for
    fully hermetic integration tests.
    
    Once it's stabilized this allows us to distribute application-specific
    toolchain containers and to share CI caches with contributors to
    eliminate redundant rebuilds.

--- flake.nix
+++ flake.nix
@@ -35,6 +35,12 @@
 
           customStdenv = import ./tools/llvmStdenv.nix { inherit pkgs; };
 
+          # TODO(aaronmondal): This doesn't work with rules_rust yet.
+          customClang = pkgs.callPackage ./tools/customClang.nix {
+            inherit pkgs;
+            stdenv = customStdenv;
+          };
+
           craneLib = crane.lib.${system};
 
           src = pkgs.lib.cleanSourceWith {
@@ -68,6 +74,8 @@
           publish-ghcr = import ./tools/publish-ghcr.nix { inherit pkgs; };
 
           local-image-test = import ./tools/local-image-test.nix { inherit pkgs; };
+
+          generate-toolchains = import ./tools/generate-toolchains.nix { inherit pkgs; };
         in
         {
           apps = {
@@ -79,6 +87,7 @@
           packages = {
             inherit publish-ghcr local-image-test;
             default = nativelink;
+            lre = import ./local-remote-execution/image.nix { inherit pkgs nativelink; };
             image = pkgs.dockerTools.streamLayeredImage {
               name = "nativelink";
               contents = [
@@ -124,6 +133,8 @@
 
               # Additional tools from within our development environment.
               local-image-test
+              generate-toolchains
+              customClang
             ] ++ maybeDarwinDeps;
             shellHook = ''
               # Generate the .pre-commit-config.yaml symlink when entering the
---
+++ local-remote-execution/examples/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+auto main() -> int {
+  std::cout << "Hello, world!" << '\n';
+  return 0;
+}
