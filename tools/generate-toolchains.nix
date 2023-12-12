6133196deb463d2a8dcc210e5df7e3c7115416b4
Aaron Siddhartha Mondal <aaron@eomii.org>
2023-12-12 18:27:26 +0100

    Introduce Local Remote Execution
    Provide generated toolchain configurations and a reproducible remote
    execution toolchain container with a C++ toolchain that can be used for
    fully hermetic integration tests.
    
    Once it's stabilized this allows us to distribute application-specific
    toolchain containers and to share CI caches with contributors to
    eliminate redundant rebuilds.

---
+++ tools/generate-toolchains.nix
@@ -0,0 +1,37 @@
+{ pkgs, }:
+
+let
+
+rbeConfigsGen = import ../local-remote-execution/rbe-configs-gen.nix {
+  inherit pkgs;
+};
+
+in
+
+pkgs.writeShellScriptBin "generate-toolchains" ''
+  #!{pkgs.bash}/bin/bash
+  set -xeuo pipefail
+
+  cd $(git rev-parse --show-toplevel)
+
+  IMAGE_TAG=$(nix eval .#lre.imageTag --raw)
+
+  $(nix build .#lre --print-build-logs --verbose) \
+    && ./result \
+    | ${pkgs.skopeo}/bin/skopeo \
+      copy \
+      docker-archive:/dev/stdin \
+      docker-daemon:native-link-toolchain:''${IMAGE_TAG}
+
+  ${rbeConfigsGen}/bin/rbe_configs_gen \
+    --toolchain_container=native-link-toolchain:''${IMAGE_TAG} \
+    --exec_os=linux \
+    --target_os=linux \
+    --bazel_version=${pkgs.bazel.version} \
+    --output_src_root=$(git rev-parse --show-toplevel) \
+    --output_config_path=local-remote-execution/generated \
+    --bazel_path=${pkgs.bazel}/bin/bazel \
+    --cpp_env_json=local-remote-execution/cpp_env.json
+
+  pre-commit run -a
+''
