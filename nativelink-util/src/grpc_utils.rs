ef4ba39fcacdaf675acb3f753e4d646069ea1adf
Chris Staite <christopher.staite@menlosecurity.com>
2024-01-31 10:49:37 +0000

    Max concurrent GrpcStore streams
    Add a configuration option to limit the number of concurrent streams that are
    active with the upstream GrpcStore.
    
    With this, we also introduce reconnection on error since Tonic doesn't do
    that.

---
+++ nativelink-util/src/grpc_utils.rs
@@ -0,0 +1,103 @@
+// Copyright 2024 The Native Link Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use async_lock::{Semaphore, SemaphoreGuard};
+use nativelink_error::{Code, Error};
+use parking_lot::Mutex;
+use tonic::transport::{Channel, Endpoint};
+
+/// A helper utility that enables management of a suite of connections to an
+/// upstream gRPC endpoint using Tonic.
+pub struct ConnectionManager {
+    /// The endpoints to establish Channels for.
+    endpoints: Vec<Endpoint>,
+    /// A balance channel over the above endpoints which is kept with a
+    /// monotonic index to ensure we only re-create a channel on the first error
+    /// received on it.
+    channel: Mutex<(usize, Channel)>,
+    /// If a maximum number of upstream requests are allowed at a time, this
+    /// is a Semaphore to manage that.
+    request_semaphore: Option<Semaphore>,
+}
+
+impl ConnectionManager {
+    /// Create a connection manager that creates a balance list between a given
+    /// set of Endpoints.  This will restrict the number of concurrent requests
+    /// assuming that the user of this connection manager uses the connection
+    /// only once and reports all errors.
+    pub fn new(endpoints: impl IntoIterator<Item = Endpoint>, max_concurrent_requests: usize) -> Self {
+        let endpoints = Vec::from_iter(endpoints);
+        let channel = Channel::balance_list(endpoints.iter().cloned());
+        Self {
+            endpoints,
+            channel: Mutex::new((0, channel)),
+            request_semaphore: (max_concurrent_requests > 0).then_some(Semaphore::new(max_concurrent_requests)),
+        }
+    }
+
+    /// Get a connection slot for an Endpoint, this contains a Channel which
+    /// should be used once and any errors should be reported back to the
+    /// on_error method to ensure that the Channel is re-connected on error.
+    pub async fn get_connection(&self) -> Connection<'_> {
+        let _permit = if let Some(semaphore) = &self.request_semaphore {
+            Some(semaphore.acquire().await)
+        } else {
+            None
+        };
+        let channel_lock = self.channel.lock();
+        Connection {
+            channel_id: channel_lock.0,
+            channel: channel_lock.1.clone(),
+            parent: self,
+            _permit,
+        }
+    }
+}
+
+/// An instance of this is obtained for every communication with the gGRPC
+/// service.  This handles the permit for limiting concurrency, and also
+/// re-connecting the underlying channel on error.  It depends on users
+/// reporting all errors.
+pub struct Connection<'a> {
+    channel: Channel,
+    channel_id: usize,
+    parent: &'a ConnectionManager,
+    _permit: Option<SemaphoreGuard<'a>>,
+}
+
+impl<'a> Connection<'a> {
+    pub fn on_error(&self, err: &Error) {
+        // Usually Tonic reconnects on upstream errors (like Unavailable) but
+        // if there are protocol errors (such as GoAway) then it will not
+        // attempt to re-connect, and therefore we are forced to manually do
+        // that.
+        if err.code != Code::Internal {
+            return;
+        }
+        // The connection is not re-esablished in Tonic, so we need to create a
+        // new connection now.
+        let mut channel_lock = self.parent.channel.lock();
+        // Channel already changed, don't do it again.
+        if channel_lock.0 != self.channel_id {
+            return;
+        }
+        // Connect a new channel in the background.
+        channel_lock.0 += 1;
+        channel_lock.1 = Channel::balance_list(self.parent.endpoints.iter().cloned());
+    }
+
+    pub fn get_channel(&self) -> Channel {
+        self.channel.clone()
+    }
+}
