81b64f73e207ad0ae2d87f531f9e93657b11ffd1
Chris Staite <137425734+chrisstaite-menlo@users.noreply.github.com>
2024-02-02 09:32:25 +0000

    Don't retry permanent failures (#634)
    The retrier always retries on error.  However, some errors are permanent, for
    example NotFound.  This means that if a request is made to get something from
    the CAS (or GrpcStore AC) which is not there, it keeps trying even though it's
    still not there.  This defines the set of errors which shoudl be retried and
    fails otherwise.

--- nativelink-util/src/retry.rs
+++ nativelink-util/src/retry.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 The Native Link Authors. All rights reserved.
+// Copyright 2023-2024 The Native Link Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,14 +18,16 @@ use std::time::Duration;
 
 use futures::future::Future;
 use futures::stream::StreamExt;
+use nativelink_config::stores::{ErrorCode, Retry};
 use nativelink_error::{make_err, Code, Error};
+use tracing::debug;
 
-pub struct ExponentialBackoff {
+struct ExponentialBackoff {
     current: Duration,
 }
 
 impl ExponentialBackoff {
-    pub fn new(base: Duration) -> Self {
+    fn new(base: Duration) -> Self {
         ExponentialBackoff { current: base }
     }
 }
@@ -40,6 +42,7 @@ impl Iterator for ExponentialBackoff {
 }
 
 type SleepFn = Arc<dyn Fn(Duration) -> Pin<Box<dyn Future<Output = ()> + Send>> + Sync + Send>;
+type JitterFn = Arc<dyn Fn(Duration) -> Duration + Send + Sync>;
 
 #[derive(PartialEq, Eq, Debug)]
 pub enum RetryResult<T> {
@@ -52,33 +55,104 @@ pub enum RetryResult<T> {
 #[derive(Clone)]
 pub struct Retrier {
     sleep_fn: SleepFn,
+    jitter_fn: JitterFn,
+    config: Retry,
+}
+
+fn to_error_code(code: &Code) -> ErrorCode {
+    match code {
+        Code::Cancelled => ErrorCode::Cancelled,
+        Code::Unknown => ErrorCode::Unknown,
+        Code::InvalidArgument => ErrorCode::InvalidArgument,
+        Code::DeadlineExceeded => ErrorCode::DeadlineExceeded,
+        Code::NotFound => ErrorCode::NotFound,
+        Code::AlreadyExists => ErrorCode::AlreadyExists,
+        Code::PermissionDenied => ErrorCode::PermissionDenied,
+        Code::ResourceExhausted => ErrorCode::ResourceExhausted,
+        Code::FailedPrecondition => ErrorCode::FailedPrecondition,
+        Code::Aborted => ErrorCode::Aborted,
+        Code::OutOfRange => ErrorCode::OutOfRange,
+        Code::Unimplemented => ErrorCode::Unimplemented,
+        Code::Internal => ErrorCode::Internal,
+        Code::Unavailable => ErrorCode::Unavailable,
+        Code::DataLoss => ErrorCode::DataLoss,
+        Code::Unauthenticated => ErrorCode::Unauthenticated,
+        _ => ErrorCode::Unknown,
+    }
 }
 
 impl Retrier {
-    pub fn new(sleep_fn: SleepFn) -> Self {
-        Retrier { sleep_fn }
+    pub fn new(sleep_fn: SleepFn, jitter_fn: JitterFn, config: Retry) -> Self {
+        Retrier {
+            sleep_fn,
+            jitter_fn,
+            config,
+        }
     }
 
-    pub fn retry<'a, T, Fut, I>(
-        &'a self,
-        duration_iter: I,
-        operation: Fut,
-    ) -> Pin<Box<dyn Future<Output = Result<T, Error>> + 'a + Send>>
+    /// This should only return true if the error code should be interpreted as
+    /// temporary.
+    fn should_retry(&self, code: &Code) -> bool {
+        if *code == Code::Ok {
+            false
+        } else if let Some(retry_codes) = &self.config.retry_on_errors {
+            retry_codes.contains(&to_error_code(code))
+        } else {
+            match code {
+                Code::InvalidArgument => false,
+                Code::FailedPrecondition => false,
+                Code::OutOfRange => false,
+                Code::Unimplemented => false,
+                Code::NotFound => false,
+                Code::AlreadyExists => false,
+                Code::PermissionDenied => false,
+                Code::Unauthenticated => false,
+                Code::Cancelled => true,
+                Code::Unknown => true,
+                Code::DeadlineExceeded => true,
+                Code::ResourceExhausted => true,
+                Code::Aborted => true,
+                Code::Internal => true,
+                Code::Unavailable => true,
+                Code::DataLoss => true,
+                _ => true,
+            }
+        }
+    }
+
+    fn get_retry_config(&self) -> impl Iterator<Item = Duration> + '_ {
+        ExponentialBackoff::new(Duration::from_millis(self.config.delay as u64))
+            .map(|d| (self.jitter_fn)(d))
+            .take(self.config.max_retries) // Remember this is number of retries, so will run max_retries + 1.
+    }
+
+    pub fn retry<'a, T, Fut>(&'a self, operation: Fut) -> Pin<Box<dyn Future<Output = Result<T, Error>> + 'a + Send>>
     where
         Fut: futures::stream::Stream<Item = RetryResult<T>> + Send + 'a,
-        I: IntoIterator<Item = Duration> + Send + 'a,
-        <I as IntoIterator>::IntoIter: Send,
         T: Send,
     {
         Box::pin(async move {
-            let mut iter = duration_iter.into_iter();
+            let mut iter = self.get_retry_config();
             let mut operation = Box::pin(operation);
+            let mut attempt = 0;
             loop {
+                attempt += 1;
                 match operation.next().await {
-                    None => return Err(make_err!(Code::Internal, "Retry stream ended abruptly",)),
+                    None => {
+                        return Err(make_err!(
+                            Code::Internal,
+                            "Retry stream ended abruptly on attempt {attempt}",
+                        ))
+                    }
                     Some(RetryResult::Ok(value)) => return Ok(value),
-                    Some(RetryResult::Err(e)) => return Err(e),
-                    Some(RetryResult::Retry(e)) => (self.sleep_fn)(iter.next().ok_or(e)?).await,
+                    Some(RetryResult::Err(e)) => return Err(e.append(format!("On attempt {attempt}"))),
+                    Some(RetryResult::Retry(e)) => {
+                        if !self.should_retry(&e.code) {
+                            debug!("Not retrying permanent error on attempt {attempt}: {e:?}");
+                            return Err(e);
+                        }
+                        (self.sleep_fn)(iter.next().ok_or(e.append(format!("On attempt {attempt}")))?).await
+                    }
                 }
             }
         })
