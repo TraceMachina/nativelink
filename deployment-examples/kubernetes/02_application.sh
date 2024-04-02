4146a341a7c0bc31a74296fcb06550f05163eceb
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2024-04-02 06:26:59 +0200

    Generalize Kubernetes worker setup (#812)
    Different worker images may now reuse the same `worker.json` config
    file. This significantly simplifies the Kubernetes setup and provides a
    clear path towards using custom worker images.
    
    The Kubernetes example is now a fairly standard kustomize application
    and should be straightforward to port to Helm charts that support remote
    execution.

--- deployment-examples/kubernetes/02_application.sh
+++ deployment-examples/kubernetes/02_application.sh
@@ -4,14 +4,6 @@
 
 KUSTOMIZE_DIR=$(git rev-parse --show-toplevel)/deployment-examples/kubernetes
 
-sed "s/__LRE_CC_TOOLCHAIN_TAG__/$(nix eval .#lre-cc.imageTag --raw)/g" \
-  "$KUSTOMIZE_DIR/worker-lre-cc.json.template" \
-  > "$KUSTOMIZE_DIR/worker-lre-cc.json" \
-
-sed "s/__LRE_JAVA_TOOLCHAIN_TAG__/$(nix eval .#lre-java.imageTag --raw)/g" \
-  "$KUSTOMIZE_DIR/worker-lre-java.json.template" \
-  > "$KUSTOMIZE_DIR/worker-lre-java.json" \
-
 kubectl apply -k "$KUSTOMIZE_DIR"
 
 kubectl rollout status deploy/nativelink-cas
