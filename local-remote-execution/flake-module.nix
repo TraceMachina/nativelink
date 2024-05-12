60f712bcddd5c2cd3d3bdd537c4cc136fe6497c7
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2024-05-12 03:51:31 +0200

    Introduce the LRE flake module (#909)
    This commit makes LRE importable into other projects.
    
    The new setup uses `flake-parts` and generator logic inspired by
    `git-hooks.nix` to create a `.bazelrc.lre` config file in repositories
    that configure the `local-remote-execution` flake module. This config
    file can be hooked up with Bazel to provide reproducible toolchains
    across different projects.
    
    The logic that generates the `.bazelrc.lre` file is largely reusable for
    future additional generator steps, such as a `.bazelrc.nativelink` to
    set up the connection to a nativelink cluster.

---
+++ local-remote-execution/flake-module.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  flake-parts-lib,
+  ...
+}: {
+  options = {
+    perSystem = flake-parts-lib.mkPerSystemOption (
+      {
+        config,
+        options,
+        pkgs,
+        ...
+      }: let
+        cfg = config.local-remote-execution;
+      in {
+        options = {
+          local-remote-execution = {
+            pkgs = lib.mkOption {
+              type = lib.types.uniq (lib.types.lazyAttrsOf (lib.types.raw or lib.types.unspecified));
+              description = lib.mdDoc ''
+                Nixpkgs to use in the local-remote-execution settings.
+              '';
+              default = pkgs;
+              defaultText = lib.literalMD "`pkgs` (module argument)";
+            };
+            settings = lib.mkOption {
+              type = lib.types.submoduleWith {
+                modules = [./modules/lre.nix];
+                specialArgs = {inherit (cfg) pkgs;};
+              };
+              default = {};
+              description = lib.mdDoc ''
+                The LRE configuration.
+              '';
+            };
+            installationScript = lib.mkOption {
+              type = lib.types.str;
+              description = lib.mdDoc "A .bazelrc.lre generator for local-remote-execution.";
+              default = cfg.settings.installationScript;
+              defaultText = lib.literalMD "bazelrc contents";
+              readOnly = true;
+            };
+          };
+        };
+      }
+    );
+  };
+}
