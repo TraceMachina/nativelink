5941018e87531c24745c9cbe428dab1f4a65c7fc
Adam Singer <adam@tracemachina.com>
2024-06-12 22:59:33 -0700

    Implementation of `ClientStateManager` where the beginnings of being able to tuck mutations behind a trait for `StateManager`. `StateManager` now wraps the `StateManagerImpl` for inner state structure `StateManagerImpl`. All mutations should be done on a lock on `inner` in the future. Moving implementation code of `add_action` and `filter_operations` into `ClientStateManager` trait. Updated accessor code from `state_manager` to `state_manager.inner`.

--- nativelink-scheduler/src/scheduler_state/mod.rs
+++ nativelink-scheduler/src/scheduler_state/mod.rs
@@ -13,6 +13,8 @@
 // limitations under the License.
 
 pub(crate) mod awaited_action;
+pub(crate) mod client_action_state_result;
 pub(crate) mod completed_action;
+pub(crate) mod metrics;
 pub(crate) mod state_manager;
 pub(crate) mod workers;
