9850d67b642b1d17d832ed5d4c43ed8580ca343b
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2024-06-16 15:36:37 +0200

    Reimplement documentation infrastructure
    This should fix most of our current documentation issues.

---
+++ docs/scripts/md_to_mdx_aot.ts
@@ -0,0 +1,81 @@
+import { transformMarkdownToMdx } from "./md_to_mdx";
+
+async function readMarkdownFile(filePath: string): Promise<string> {
+  try {
+    return await Bun.file(filePath).text();
+  } catch (err) {
+    console.error(`Error reading file: ${err}`);
+    throw err;
+  }
+}
+
+async function writeMdxFile(
+  outputFilePath: string,
+  mdxContent: string,
+): Promise<void> {
+  try {
+    await Bun.write(outputFilePath, mdxContent);
+    console.info(`Generated: ${outputFilePath}`);
+  } catch (err) {
+    console.error(`Error writing file: ${err}`);
+    throw err;
+  }
+}
+
+async function convertMarkdownToMdx(
+  filePath: string,
+  outputFilePath: string,
+  description: string,
+  pagefind = true,
+): Promise<void> {
+  try {
+    const markdown = await readMarkdownFile(filePath);
+    const mdxContent = await transformMarkdownToMdx(
+      markdown,
+      description,
+      pagefind,
+    );
+    await writeMdxFile(outputFilePath, mdxContent);
+  } catch (err) {
+    console.error(`Error during conversion: ${err}`);
+    throw err;
+  }
+}
+
+// Convert the actual files.
+convertMarkdownToMdx(
+  "../local-remote-execution/README.md",
+  "src/content/docs/explanations/lre.mdx",
+  "Local Remote Execution architecture",
+);
+convertMarkdownToMdx(
+  "../CONTRIBUTING.md",
+  "src/content/docs/guides/contributing.mdx",
+  "NativeLink contribution guidelines",
+);
+convertMarkdownToMdx(
+  "../nativelink-config/README.md",
+  "src/content/docs/guides/configuration.mdx",
+  "NativeLink configuration guide",
+);
+convertMarkdownToMdx(
+  "../deployment-examples/chromium/README.md",
+  "src/content/docs/guides/chromium.mdx",
+  "NativeLink deployment example for Chromium",
+);
+convertMarkdownToMdx(
+  "../deployment-examples/kubernetes/README.md",
+  "src/content/docs/guides/kubernetes.mdx",
+  "NativeLink deployment example for Kubernetes",
+);
+convertMarkdownToMdx(
+  "../CHANGELOG.md",
+  "src/content/docs/reference/changelog.mdx",
+  "NativeLink's Changelog",
+  false, // Set pagefind to false for changelog
+);
+convertMarkdownToMdx(
+  "../README.md",
+  "src/content/docs/tutorials/setup.mdx",
+  "Get starget with NativeLink",
+);
