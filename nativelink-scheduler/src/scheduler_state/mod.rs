bf2dc93b17514b145b22d86f654dd10bd33fc573
Zach Birenbaum <zacharyobirenbaum@gmail.com>
2024-07-01 13:36:38 -0700

    Generalize state manager helper structs.
    Moves and changes the helper structs for RedisStateManager so they can
    be reused by other StateManagers such as MemoryStateManager.

--- nativelink-scheduler/src/scheduler_state/mod.rs
+++ nativelink-scheduler/src/scheduler_state/mod.rs
@@ -17,5 +17,7 @@ pub(crate) mod client_action_state_result;
 pub(crate) mod completed_action;
 pub(crate) mod matching_engine_action_state_result;
 pub(crate) mod metrics;
+pub(crate) mod operation_state;
+pub(crate) mod redis_state_manager;
 pub(crate) mod state_manager;
 pub(crate) mod workers;
