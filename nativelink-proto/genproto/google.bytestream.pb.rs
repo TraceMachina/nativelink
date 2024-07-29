d8c407a973a268e9a45078f2d5fe873f3e33b050
Nathan (Blaise) Bruer <github.blaise@allada.com>
2024-07-28 20:33:41 -0500

    Make Cargo.toml feature pins compatible with project/main (#1212)
    A fix outlined here:
    https://github.com/ToBinio/cargo-features-manager/issues/37
    
    This change is to make nativelink compatible with those changes.

--- nativelink-proto/genproto/google.bytestream.pb.rs
+++ nativelink-proto/genproto/google.bytestream.pb.rs
@@ -143,17 +143,6 @@ pub mod byte_stream_client {
     pub struct ByteStreamClient<T> {
         inner: tonic::client::Grpc<T>,
     }
-    impl ByteStreamClient<tonic::transport::Channel> {
-        /// Attempt to create a new client by connecting to a given endpoint.
-        pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
-        where
-            D: TryInto<tonic::transport::Endpoint>,
-            D::Error: Into<StdError>,
-        {
-            let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
-            Ok(Self::new(conn))
-        }
-    }
     impl<T> ByteStreamClient<T>
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
--- nativelink-proto/genproto/google.longrunning.pb.rs
+++ nativelink-proto/genproto/google.longrunning.pb.rs
@@ -180,17 +180,6 @@ pub mod operations_client {
     pub struct OperationsClient<T> {
         inner: tonic::client::Grpc<T>,
     }
-    impl OperationsClient<tonic::transport::Channel> {
-        /// Attempt to create a new client by connecting to a given endpoint.
-        pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
-        where
-            D: TryInto<tonic::transport::Endpoint>,
-            D::Error: Into<StdError>,
-        {
-            let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
-            Ok(Self::new(conn))
-        }
-    }
     impl<T> OperationsClient<T>
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
