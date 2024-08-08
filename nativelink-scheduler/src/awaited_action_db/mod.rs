d5f2781eff92432ceea9497f7b1fe1c3b672eda4
Zach Birenbaum <zacharyobirenbaum@gmail.com>
2024-08-08 12:00:11 -0700

    Conversion implementations for awaited action db structs (#1243)
    Implements a number of convenience implementations to simplify
    compatibility with fred and conversions into redis keys/values.

--- nativelink-scheduler/src/awaited_action_db/mod.rs
+++ nativelink-scheduler/src/awaited_action_db/mod.rs
@@ -18,9 +18,10 @@ use std::sync::Arc;
 
 pub use awaited_action::{AwaitedAction, AwaitedActionSortKey};
 use futures::{Future, Stream};
-use nativelink_error::Error;
+use nativelink_error::{make_input_err, Error, ResultExt};
 use nativelink_metric::MetricsComponent;
-use nativelink_util::action_messages::{ActionInfo, OperationId};
+use nativelink_util::action_messages::{ActionInfo, ActionStage, OperationId};
+use serde::{Deserialize, Serialize};
 
 mod awaited_action;
 
@@ -33,8 +34,38 @@ pub enum SortedAwaitedActionState {
     Completed,
 }
 
+impl TryFrom<&ActionStage> for SortedAwaitedActionState {
+    type Error = Error;
+    fn try_from(value: &ActionStage) -> Result<Self, Error> {
+        match value {
+            ActionStage::CacheCheck => Ok(Self::CacheCheck),
+            ActionStage::Executing => Ok(Self::Executing),
+            ActionStage::Completed(_) => Ok(Self::Completed),
+            ActionStage::Queued => Ok(Self::Queued),
+            _ => Err(make_input_err!("Invalid State")),
+        }
+    }
+}
+
+impl TryFrom<ActionStage> for SortedAwaitedActionState {
+    type Error = Error;
+    fn try_from(value: ActionStage) -> Result<Self, Error> {
+        Self::try_from(&value)
+    }
+}
+
+impl std::fmt::Display for SortedAwaitedActionState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SortedAwaitedActionState::CacheCheck => write!(f, "CacheCheck"),
+            SortedAwaitedActionState::Queued => write!(f, "Queued"),
+            SortedAwaitedActionState::Executing => write!(f, "Executing"),
+            SortedAwaitedActionState::Completed => write!(f, "Completed"),
+        }
+    }
+}
 /// A struct pointing to an AwaitedAction that can be sorted.
-#[derive(Debug, Clone, MetricsComponent)]
+#[derive(Debug, Clone, Serialize, Deserialize, MetricsComponent)]
 pub struct SortedAwaitedAction {
     #[metric(help = "The sort key of the AwaitedAction")]
     pub sort_key: AwaitedActionSortKey,
@@ -61,6 +92,48 @@ impl Ord for SortedAwaitedAction {
         self.sort_key
             .cmp(&other.sort_key)
             .then_with(|| self.operation_id.cmp(&other.operation_id))
+    }
+}
+
+impl std::fmt::Display for SortedAwaitedAction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::write(
+            f,
+            format_args!("{}-{}", self.sort_key.as_u64(), self.operation_id),
+        )
+    }
+}
+
+impl From<&AwaitedAction> for SortedAwaitedAction {
+    fn from(value: &AwaitedAction) -> Self {
+        Self {
+            operation_id: value.operation_id().clone(),
+            sort_key: value.sort_key(),
+        }
+    }
+}
+
+impl From<AwaitedAction> for SortedAwaitedAction {
+    fn from(value: AwaitedAction) -> Self {
+        Self::from(&value)
+    }
+}
+
+impl TryInto<Vec<u8>> for SortedAwaitedAction {
+    type Error = Error;
+    fn try_into(self) -> Result<Vec<u8>, Self::Error> {
+        serde_json::to_vec(&self)
+            .map_err(|e| make_input_err!("{}", e.to_string()))
+            .err_tip(|| "In SortedAwaitedAction::TryInto::<Vec<u8>>")
+    }
+}
+
+impl TryFrom<&[u8]> for SortedAwaitedAction {
+    type Error = Error;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        serde_json::from_slice(value)
+            .map_err(|e| make_input_err!("{}", e.to_string()))
+            .err_tip(|| "In AwaitedAction::TryFrom::&[u8]")
     }
 }
 
