d6eb91b4284c4600cc9c5f0260bef135a12c392a
Adam Singer <adam@tracemachina.com>
2024-08-09 17:29:14 +0000

    Append buck2 toolchain with additional packages
    A toolchain for buck2 depends on more than llvm and rust,
    appending more packages for compiling buck2 using remote
    execution on nativelink. This mirrors similar to what is
    used in [buck2/flake.nix](https://github.com/facebook/buck2/blob/main/flake.nix)
    
    `create-worker-experimental.nix` now flattens out the container
    image layout to a standard unix filesystem layout. Using the
    nix store layout requires client knowledge of toolchains and
    a similar setup as rbe gen/local remote execution which isn't
    available at the moment.

--- flake.nix
+++ flake.nix
@@ -269,10 +269,8 @@
         createWorker = pkgs.callPackage ./tools/create-worker.nix {inherit buildImage self;};
         buck2-toolchain = let
           buck2-nightly-rust-version = "2024-04-28";
-          buck2-nightly-rust =
-            if pkgs.stdenv.isDarwin
-            then pkgs.rust-bin.nightly.${buck2-nightly-rust-version}
-            else pkgs.pkgsMusl.rust-bin.nightly.${buck2-nightly-rust-version};
+          buck2-nightly-rust = pkgs.rust-bin.nightly.${buck2-nightly-rust-version};
+          buck2-rust = buck2-nightly-rust.default.override {extensions = ["rust-src"];};
         in
           pkgs.callPackage ./tools/create-worker-experimental.nix {
             inherit buildImage self;
@@ -280,12 +278,19 @@
             packagesForImage = [
               pkgs.coreutils
               pkgs.bash
-              pkgs.clang
               pkgs.go
               pkgs.diffutils
               pkgs.gnutar
               pkgs.gzip
-              buck2-nightly-rust.default
+              pkgs.python3Full
+              pkgs.unzip
+              pkgs.zstd
+              pkgs.cargo-bloat
+              pkgs.mold-wrapped
+              pkgs.reindeer
+              pkgs.lld_16
+              pkgs.clang_16
+              buck2-rust
             ];
           };
         siso-chromium = buildImage {
