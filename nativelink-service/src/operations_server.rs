ae7981f8f283bdc0623262ef66dcfebd7449c0c4
Cass Fridkin <cass@swag.lgbt>
2024-09-24 22:16:53 -0600

    implement "get"

--- nativelink-service/src/operations_server.rs
+++ nativelink-service/src/operations_server.rs
@@ -26,7 +26,7 @@ use nativelink_proto::google::longrunning::{
     ListOperationsResponse, Operation, WaitOperationRequest,
 };
 use nativelink_proto::google::rpc;
-use nativelink_util::action_messages::ActionStage;
+use nativelink_util::action_messages::{ActionStage, OperationId};
 use nativelink_util::operation_state_manager::{
     ActionStateResult, ClientStateManager, OperationFilter,
 };
@@ -101,54 +101,41 @@ impl OpsServer {
 
         let mut out = Vec::with_capacity(action_state_results.len());
         for action_state_result in action_state_results {
-            let info = action_state_result.as_action_info().await?;
-            let state = action_state_result.as_state().await?;
-
-            let name = info.unique_qualifier.digest().to_string();
-            let metadata = None;
-            let (done, result) = match &state.stage {
-                ActionStage::Completed(action_result) => {
-                    let result = if action_result.exit_code == 0 {
-                        OperationResult::Response(Any::default())
-                    } else {
-                        OperationResult::Error(rpc::Status {
-                            code: action_result.exit_code,
-                            message: action_result.message.clone(),
-                            details: vec![],
-                        })
-                    };
-
-                    (true, Some(result))
-                }
-                ActionStage::CompletedFromCache(cached_action_result) => {
-                    let result = if cached_action_result.exit_code == 0 {
-                        OperationResult::Response(Any::default())
-                    } else {
-                        OperationResult::Error(rpc::Status {
-                            code: cached_action_result.exit_code,
-                            message: String::from_utf8_lossy(&cached_action_result.stderr_raw)
-                                .into_owned(),
-                            details: vec![],
-                        })
-                    };
-
-                    (true, Some(result))
-                }
-                _ => (false, None),
-            };
-
-            out.push(Operation {
-                name,
-                metadata,
-                done,
-                result,
-            });
+            let operation = translate_action_stage_result(action_state_result).await?;
+            out.push(operation);
         }
 
         Ok(ListOperationsResponse {
             operations: out,
             next_page_token,
         })
+    }
+
+    async fn get_operation_inner(
+        &self,
+        client_operation_id: OperationId,
+    ) -> Result<Operation, Error> {
+        for scheduler in self.schedulers.values() {
+            if let Some(action_state_result) = scheduler
+                .filter_operations(OperationFilter {
+                    client_operation_id: Some(client_operation_id.clone()),
+                    ..Default::default()
+                })
+                .await?
+                .next()
+                .await
+            {
+                return translate_action_stage_result(action_state_result).await;
+            }
+        }
+
+        Err(Error::new(
+            Code::NotFound,
+            format!(
+                "Couldn't find operation with ID {}",
+                client_operation_id.into_string()
+            ),
+        ))
     }
 }
 
@@ -247,7 +234,9 @@ impl Operations for OpsServer {
         &self,
         request: Request<GetOperationRequest>,
     ) -> Result<Response<Operation>, Status> {
-        Err(Status::unimplemented("todo"))
+        let GetOperationRequest { name } = request.into_inner();
+        let message = self.get_operation_inner(OperationId::String(name)).await?;
+        Ok(Response::new(message))
     }
     /// Deletes a long-running operation. This method indicates that the client is
     /// no longer interested in the operation result. It does not cancel the
@@ -290,4 +279,50 @@ impl Operations for OpsServer {
     ) -> Result<Response<Operation>, Status> {
         Err(Status::unimplemented("todo"))
     }
+}
+
+async fn translate_action_stage_result(
+    action_state_result: Box<dyn ActionStateResult>,
+) -> Result<Operation, Error> {
+    let info = action_state_result.as_action_info().await?;
+    let state = action_state_result.as_state().await?;
+
+    let name = info.unique_qualifier.digest().to_string();
+    let metadata = None;
+    let (done, result) = match &state.stage {
+        ActionStage::Completed(action_result) => {
+            let result = if action_result.exit_code == 0 {
+                OperationResult::Response(Any::default())
+            } else {
+                OperationResult::Error(rpc::Status {
+                    code: action_result.exit_code,
+                    message: action_result.message.clone(),
+                    details: vec![],
+                })
+            };
+
+            (true, Some(result))
+        }
+        ActionStage::CompletedFromCache(cached_action_result) => {
+            let result = if cached_action_result.exit_code == 0 {
+                OperationResult::Response(Any::default())
+            } else {
+                OperationResult::Error(rpc::Status {
+                    code: cached_action_result.exit_code,
+                    message: String::from_utf8_lossy(&cached_action_result.stderr_raw).into_owned(),
+                    details: vec![],
+                })
+            };
+
+            (true, Some(result))
+        }
+        _ => (false, None),
+    };
+
+    Ok(Operation {
+        name,
+        metadata,
+        done,
+        result,
+    })
 }
