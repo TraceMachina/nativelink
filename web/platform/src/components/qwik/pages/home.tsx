e3a3b34d8c32891d581cc2a1e64ddf1ec0af0c7c
Schahin Rouhanizadeh <schahin@rouhanizadeh.de>
2024-11-02 02:42:47 +0100

    Add Aaron's awesome talk to homepage and resource page

--- web/platform/src/components/qwik/pages/home.tsx
+++ web/platform/src/components/qwik/pages/home.tsx
@@ -14,6 +14,18 @@ export const LandingPage = component$(() => {
       <Testimonial />
       <Features />
       <Engineers />
+      <div class="mx-auto left-0 right-0 w-9/11 flex justify-center items-center flex-col gap-4">
+        <div class="text-sm flex justify-center w-full md:w-9/12 px-8 md:px-0">An awesome talk of one of TraceMachina's leading engineers Aaron Mondal</div>
+        <div class="flex justify-center w-full md:w-9/12 px-8 md:px-0 flex justify-center items-center pt-4 pb-12">
+          <div class="w-full max-w-4xl aspect-video">
+            <iframe
+              class="w-full h-full"
+              src="https://www.youtube.com/embed/uokjTev8myk?rel=0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            ></iframe>
+          </div>
+        </div>
+      </div>
       <Benefits />
       <Community />
     </main>
