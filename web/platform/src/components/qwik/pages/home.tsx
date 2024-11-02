e436edcce4cd8f47f89a34c8ab7d6e9db2204d32
Schahin Rouhanizadeh <schahin@rouhanizadeh.de>
2024-11-02 02:42:47 +0100

    Add Aaron's awesome talk to homepage and resource page
    Add an iframe to the homepage and resource page linked to the the youtube video:
    "Hermetic Toolchain Creation with Local Remote Execution (LRE) & Nix" by Aaron Mondal, NativeLink

--- web/platform/src/components/qwik/pages/home.tsx
+++ web/platform/src/components/qwik/pages/home.tsx
@@ -14,6 +14,18 @@ export const LandingPage = component$(() => {
       <Testimonial />
       <Features />
       <Engineers />
+      <div class="mx-auto left-0 right-0 w-9/11 flex justify-center items-center flex-col gap-4">
+        <div class="flex justify-center w-full md:w-9/12 px-8 md:px-0 flex justify-center items-center pt-4 pb-12">
+          <div class="w-full max-w-4xl aspect-video">
+            <iframe
+              title="Hermetic Toolchain Creation with Local Remote Execution (LRE) & Nix - Aaron Mondal, NativeLink"
+              class="w-full h-full"
+              src="https://www.youtube.com/embed/uokjTev8myk?rel=0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            />
+          </div>
+        </div>
+      </div>
       <Benefits />
       <Community />
     </main>
