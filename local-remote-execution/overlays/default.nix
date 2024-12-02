a28c49240cda00fe8295190337e584005891ab72
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2024-12-02 23:11:47 +0100

    Implement Local Remote Execution for Rust
    This change implements ~40 Rust toolchain configurations that reuse
    Nix's executables and make them available via Bazel.
    
    The new setup is fully reproducible, supports musl and glibc and
    works from all nix-supported hosts to all sensible crosscompilation
    targets seamlessly through remote and local execution.
    
    Until we have the actual infrastructure set up to fully test all
    configurations we have these 12 which are practically usable
    immediately:
    
    - MacOS native, aarch64 and x86_64, stable and nightly
    - Linux native, aarch64 and x64_64, stable and nightly, glibc and musl
    
    The channel is configurable via a new
    `--@local-remote-execution//lre-rs:channel` flag while the libc version
    is configurable via the target platform. For instance:
    
    ```bash
    bazel build \
        nativelink \
        --@local-remote-execution//lre-rs:channel=nightly \
        --platforms=@local-remote-execution//lre-rs/platforms:x86_64-unknown-linux-gnu
    
    bazel build \
        nativelink \
        --platforms=@local-remote-execution//lre-rs/platforms:x86_64-unknown-linux-musl
    ```

--- local-remote-execution/overlays/default.nix
+++ local-remote-execution/overlays/default.nix
@@ -1,12 +1,46 @@
-final: _prev: {
-  lre = {
-    stdenv = final.callPackage ./stdenv.nix {
-      llvmPackages = final.llvmPackages_19;
-      targetPackages = final;
-    };
+{nix2container}: final: _prev: {
+  inherit (nix2container.packages.${final.system}) nix2container;
 
-    clang = final.callPackage ./clang.nix {
-      inherit (final.lre) stdenv;
-    };
+  rbe-configs-gen = final.callPackage ./rbe-configs-gen {};
+
+  rbe-autogen = final.callPackage ./rbe-autogen.nix {
+    inherit (final.lre) stdenv;
   };
+
+  lre =
+    {
+      stdenv = final.callPackage ./stdenv.nix {
+        llvmPackages = final.llvmPackages_19;
+        targetPackages = final;
+      };
+
+      clang = final.callPackage ./clang.nix {
+        inherit (final.lre) stdenv;
+      };
+    }
+    // (let
+      rustConfig = import ./rust-config.nix;
+
+      stableRustFor = p: (rustConfig.mkRust {
+        execPkgs = p;
+        channel = "stable";
+      });
+
+      nightlyRustFor = p: (rustConfig.mkRust {
+        execPkgs = p;
+        channel = "nightly";
+        extensions = ["llvm-tools"];
+      });
+
+      stable-rust = stableRustFor final;
+      nightly-rust = nightlyRustFor final;
+    in {
+      inherit stable-rust nightly-rust stableRustFor nightlyRustFor;
+
+      lre-rs = final.callPackage ./lre-rs.nix {
+        inherit (rustConfig) nixSystemToRustTargets;
+      };
+
+      lre-cc = final.callPackage ./lre-cc.nix {};
+    });
 }
