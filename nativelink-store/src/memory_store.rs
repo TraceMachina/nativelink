86078169ebfdf1e96370601949ca222d2b80dbde
Blaise Bruer <github.blaise@allada.com>
2025-02-17 02:21:46 -0600

    MemoryStore now uses fixed memory pool per store
    MemoryStore now pre-allocates max_bytes from the eviction policy,
    then installs a custom allocator in that pool that the store will
    use. In testing, memory fragmentation appears to have dropped
    significantly decreasing the number of sys-calls managing memory
    and slightly reduced user time.
    
    In addition, we remove two full copies of our data when we store
    it in the memory store.
    
    This PR also adds two new crates (likely to be published).
    
    "Heap Allocator"
      Fixed heap allocator based on the TLSF (Two-Level Segregated
      Fit) algorithm. TLSF is a fast, constant-time memory allocation
      algorithm designed for real-time applications. It organizes free
      memory into segregated lists based on block sizes, allowing for
      quick allocation and deallocation with minimal fragmentation.
    
    "Alloc Bytes"
      Provides custom byte buffers allocated using a user-provided
      allocator. It offers both mutable and immutable byte buffers,
      enabling efficient memory allocation strategies in systems with
      custom memory requirements.
    
    closes: #289

--- nativelink-store/src/memory_store.rs
+++ nativelink-store/src/memory_store.rs
@@ -14,15 +14,18 @@
 
 use std::borrow::Borrow;
 use std::fmt::Debug;
+use std::mem::MaybeUninit;
 use std::ops::Bound;
 use std::pin::Pin;
 use std::sync::Arc;
 use std::time::SystemTime;
 
+use alloc_bytes::{AllocBytesError, AllocBytesMut};
 use async_trait::async_trait;
-use bytes::{Bytes, BytesMut};
+use bytes::Bytes;
+use heap_allocator::MutexOwnedFixedHeapAllocator;
 use nativelink_config::stores::MemorySpec;
-use nativelink_error::{Code, Error, ResultExt};
+use nativelink_error::{make_err, Code, Error, ResultExt};
 use nativelink_metric::MetricsComponent;
 use nativelink_util::buf_channel::{DropCloserReadHalf, DropCloserWriteHalf};
 use nativelink_util::evicting_map::{EvictingMap, LenEntry};
@@ -30,11 +33,18 @@ use nativelink_util::health_utils::{
     default_health_status_indicator, HealthRegistryBuilder, HealthStatusIndicator,
 };
 use nativelink_util::store_trait::{StoreDriver, StoreKey, StoreKeyBorrow, UploadSizeInfo};
+use tracing::{event, Level};
 
 use crate::cas_utils::is_zero_digest;
 
+/// Default amount of memory to evict when the cache is full.
+const DEFAULT_EVICT_BYTES: usize = 2 * 1024 * 1024; // 4MB.
+
+/// The maximum size of memory that can be allocated in a single call.
+const MEMORY_ALLOC_CHUNK_SIZE: usize = 4 * 1024 * 1024; // 4MB.
+
 #[derive(Clone)]
-pub struct BytesWrapper(Bytes);
+struct BytesWrapper(Bytes);
 
 impl Debug for BytesWrapper {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -58,19 +68,36 @@ impl LenEntry for BytesWrapper {
 pub struct MemoryStore {
     #[metric(group = "evicting_map")]
     evicting_map: EvictingMap<StoreKeyBorrow, BytesWrapper, SystemTime>,
+    heap_allocator: Arc<MutexOwnedFixedHeapAllocator<Vec<MaybeUninit<u8>>>>,
 }
 
 impl MemoryStore {
     pub fn new(spec: &MemorySpec) -> Arc<Self> {
-        let empty_policy = nativelink_config::stores::EvictionPolicy::default();
-        let eviction_policy = spec.eviction_policy.as_ref().unwrap_or(&empty_policy);
+        let mut eviction_policy = spec.eviction_policy.clone().unwrap_or_default();
+        if eviction_policy.evict_bytes == 0 {
+            eviction_policy.evict_bytes = DEFAULT_EVICT_BYTES;
+        }
+        if eviction_policy.max_bytes == 0 {
+            eviction_policy.max_bytes = DEFAULT_EVICT_BYTES * 10;
+            event!(
+                Level::WARN,
+                "MemoryStore config' max_bytes is 0, setting to 10x evict_bytes({}). This will be required to be set in a future release.",
+                eviction_policy.max_bytes,
+            );
+        }
+        let max_bytes = eviction_policy.max_bytes;
+        let mut data = Vec::<MaybeUninit<u8>>::with_capacity(max_bytes);
+        // Safety: We are setting the length of the vector to the maximum
+        unsafe { data.set_len(max_bytes) };
+        let heap_allocator = Arc::new(MutexOwnedFixedHeapAllocator::new(data));
         Arc::new(Self {
-            evicting_map: EvictingMap::new(eviction_policy, SystemTime::now()),
+            evicting_map: EvictingMap::new(&eviction_policy, SystemTime::now()),
+            heap_allocator,
         })
     }
 
-    /// Returns the number of key-value pairs that are currently in the the cache.
-    /// Function is not for production code paths.
+    /// Returns the number of key-value pairs currently in the cache.
+    /// (Not used in production.)
     pub async fn len_for_test(&self) -> usize {
         self.evicting_map.len_for_test().await
     }
@@ -125,18 +152,82 @@ impl StoreDriver for MemoryStore {
         self: Pin<&Self>,
         key: StoreKey<'_>,
         mut reader: DropCloserReadHalf,
-        _size_info: UploadSizeInfo,
+        size_info: UploadSizeInfo,
     ) -> Result<(), Error> {
         // Internally Bytes might hold a reference to more data than just our data. To prevent
         // this potential case, we make a full copy of our data for long-term storage.
         let final_buffer = {
-            let buffer = reader
-                .consume(None)
-                .await
-                .err_tip(|| "Failed to collect all bytes from reader in memory_store::update")?;
-            let mut new_buffer = BytesMut::with_capacity(buffer.len());
-            new_buffer.extend_from_slice(&buffer[..]);
-            new_buffer.freeze()
+            let max_size = match size_info {
+                UploadSizeInfo::ExactSize(size) | UploadSizeInfo::MaxSize(size) => {
+                    if size > self.evicting_map.max_bytes() {
+                        event!(
+                            Level::WARN,
+                            "Uploaded object too large {size} > {} in memory store, suggest wrapping in a size_partitioning store to ensure big files don't get sent to this store.",
+                            self.evicting_map.max_bytes()
+                        );
+                        reader.drain().await.err_tip(|| "Failed to drain reader in memory_store::update")?;
+                        return Ok(());
+                    }
+                    usize::try_from(size)
+                }
+            }
+            .err_tip(|| "Could not convert size to usize in memory_store::update")?;
+
+            let mut alloc_bytes_mut = AllocBytesMut::new_unaligned(self.heap_allocator.clone());
+            loop {
+                let chunk = reader
+                    .recv()
+                    .await
+                    .err_tip(|| "Failed to receive data in memory_store::update")?;
+                if chunk.is_empty() {
+                    break; // EOF.
+                }
+                let new_size = alloc_bytes_mut.len().saturating_add(chunk.len());
+                if new_size > alloc_bytes_mut.capacity() {
+                    let additional_reserve = max_size
+                        .checked_sub(alloc_bytes_mut.len())
+                        .err_tip(|| "More bytes received than stated in memory store")?
+                        .min(MEMORY_ALLOC_CHUNK_SIZE);
+                    loop {
+                        match alloc_bytes_mut.reserve(additional_reserve) {
+                            Ok(()) => break,
+                            Err(AllocBytesError::FailedToRealloc) => {
+                                let evict_bytes = u64::try_from(chunk.len())
+                                    .err_tip(|| "Could not convert chunk.len() to u64")?;
+                                if self.evicting_map.force_eviction_bytes(evict_bytes).await == 0 {
+                                    event!(
+                                        Level::WARN,
+                                        "FailedToRealloc: Object too large ({max_size}) to fit in memory store even after evicting. {}",
+                                        "Suggest using a size_partitioning store to ensure big files don't get sent to this store.",
+                                    );
+                                    return Ok(());
+                                }
+                            }
+                            Err(AllocBytesError::ReserveTooLarge) => {
+                                event!(
+                                    Level::WARN,
+                                    "ReserveTooLarge: Object too large ({max_size}) to fit in memory store even after evicting. {}",
+                                    "Suggest using a size_partitioning store to ensure big files don't get sent to this store.",
+                                );
+                                return Ok(());
+                            }
+                            Err(AllocBytesError::Layout(e)) => {
+                                return Err(make_err!(
+                                    Code::Internal,
+                                    "Memory store Invalid layout. This should never happen - {e:?}"
+                                ));
+                            }
+                        }
+                    }
+                }
+                alloc_bytes_mut
+                    .extend_from_slice(&chunk)
+                    .map_err(|e| make_err!(
+                        Code::Internal,
+                        "Could not allocate bytes from slice in memory store. This should never happen - {e:?}")
+                    )?;
+            }
+            Bytes::from_owner(alloc_bytes_mut.freeze())
         };
 
         self.evicting_map
