1de4ee4b7f5a607fa559b5abcf313ab31f6e0dbc
Tom Parker-Shemilt <palfrey@tevp.net>
2025-03-24 22:11:54 +0000

    Initial Remote Asset support

---
+++ tools/buildstream/bst.nix
@@ -0,0 +1,48 @@
+# TODO(palfrey): Replace with upstream now https://github.com/NixOS/nixpkgs/pull/383451 is in
+{
+  pythonPkgs,
+  pkgs,
+  pyroaring,
+}:
+pythonPkgs.buildPythonApplication rec {
+  pname = "buildstream";
+  version = "2.4.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "apache";
+    repo = "buildstream";
+    tag = version;
+    hash = "sha256-6a0VzYO5yj7EHvAb0xa4xZ0dgBKjFcwKv2F4o93oahY=";
+  };
+
+  doCheck = false;
+
+  dependencies = [
+    pyroaring
+    pythonPkgs.click
+    pythonPkgs.grpcio
+    pythonPkgs.jinja2
+    pythonPkgs.pluginbase
+    pythonPkgs.protobuf
+    pythonPkgs.psutil
+    pythonPkgs.ruamel-yaml
+    pythonPkgs.ruamel-yaml-clib
+    pythonPkgs.ujson
+    pkgs.buildbox
+  ];
+
+  build-system = [
+    pythonPkgs.setuptools
+    pythonPkgs.cython
+  ];
+
+  meta = {
+    description = "Powerful software integration tool";
+    downloadPage = "https://buildstream.build/install.html";
+    homepage = "https://buildstream.build";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.linux;
+    mainProgram = "bst";
+  };
+}
---
+++ tools/buildstream/pyroaring.nix
@@ -0,0 +1,28 @@
+# TODO(palfrey): Replace with upstream now https://github.com/NixOS/nixpkgs/pull/383451 is in
+{pythonPkgs}:
+pythonPkgs.buildPythonPackage rec {
+  pname = "pyroaring";
+  version = "1.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Ezibenroc";
+    repo = "PyRoaringBitMap";
+    tag = version;
+    hash = "sha256-pnANvqyQ5DpG4NWSgWkAkXvSNLO67nfa97nEz3fYf1Y=";
+  };
+
+  doCheck = false;
+
+  build-system = [
+    pythonPkgs.setuptools
+    pythonPkgs.cython
+  ];
+
+  meta = {
+    description = "Python library for handling efficiently sorted integer sets";
+    homepage = "https://github.com/Ezibenroc/PyRoaringBitMap";
+    changelog = "https://github.com/Ezibenroc/PyRoaringBitMap/releases/tag/${version}";
+    license = lib.licenses.mit;
+  };
+}
