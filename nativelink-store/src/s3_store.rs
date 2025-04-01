a5e845ce3d41832f158ecf91ab3598921ba5ae75
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2025-04-01 18:26:47 +0200

    Migrate S3Store to hyper 1.x (#1639)

--- nativelink-store/src/s3_store.rs
+++ nativelink-store/src/s3_store.rs
@@ -13,34 +13,45 @@
 // limitations under the License.
 
 use std::borrow::Cow;
+use std::cmp;
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
 use std::task::{Context, Poll};
 use std::time::Duration;
-use std::{cmp, env};
 
 use async_trait::async_trait;
 use aws_config::default_provider::credentials;
+use aws_config::provider_config::ProviderConfig;
+use aws_config::retry::ErrorKind::TransientError;
 use aws_config::{AppName, BehaviorVersion};
 use aws_sdk_s3::config::Region;
 use aws_sdk_s3::operation::create_multipart_upload::CreateMultipartUploadOutput;
 use aws_sdk_s3::operation::get_object::GetObjectError;
 use aws_sdk_s3::operation::head_object::HeadObjectError;
-use aws_sdk_s3::primitives::{ByteStream, SdkBody};
+use aws_sdk_s3::primitives::{ByteStream /* SdkBody */};
 use aws_sdk_s3::types::builders::{CompletedMultipartUploadBuilder, CompletedPartBuilder};
 use aws_sdk_s3::Client;
-use aws_smithy_runtime::client::http::hyper_014::HyperClientBuilder;
-use aws_smithy_types::checksum_config::{RequestChecksumCalculation, ResponseChecksumValidation};
-use bytes::Bytes;
+use aws_smithy_runtime_api::client::http::{
+    HttpClient as SmithyHttpClient, HttpConnector as SmithyHttpConnector, HttpConnectorFuture,
+    HttpConnectorSettings, SharedHttpConnector,
+};
+use aws_smithy_runtime_api::client::orchestrator::HttpRequest;
+use aws_smithy_runtime_api::client::result::ConnectorError;
+use aws_smithy_runtime_api::client::runtime_components::RuntimeComponents;
+use aws_smithy_runtime_api::http::Response;
+use aws_smithy_types::body::SdkBody;
+use bytes::{Bytes, BytesMut};
 use futures::future::FusedFuture;
 use futures::stream::{unfold, FuturesUnordered};
 use futures::{FutureExt, Stream, StreamExt, TryFutureExt, TryStreamExt};
 use http_body::{Frame, SizeHint};
-use hyper::client::connect::{Connected, Connection, HttpConnector};
-use hyper::service::Service;
-use hyper::Uri;
-use hyper_rustls::{HttpsConnector, MaybeHttpsStream};
+use http_body_util::BodyExt;
+use hyper::{Method, Request};
+use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
+use hyper_util::client::legacy::connect::HttpConnector as LegacyHttpConnector;
+use hyper_util::client::legacy::Client as LegacyClient;
+use hyper_util::rt::TokioExecutor;
 use nativelink_config::stores::S3Spec;
 // Note: S3 store should be very careful about the error codes it returns
 // when in a retryable wrapper. Always prefer Code::Aborted or another
@@ -57,9 +68,7 @@ use nativelink_util::instant_wrapper::InstantWrapper;
 use nativelink_util::retry::{Retrier, RetryResult};
 use nativelink_util::store_trait::{StoreDriver, StoreKey, UploadSizeInfo};
 use rand::Rng;
-use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
-use tokio::net::TcpStream;
-use tokio::sync::{mpsc, SemaphorePermit};
+use tokio::sync::mpsc;
 use tokio::time::sleep;
 use tracing::{event, Level};
 
@@ -85,65 +94,16 @@ const DEFAULT_MAX_RETRY_BUFFER_PER_REQUEST: usize = 5 * 1024 * 1024; // 5MB.
 // Note: If you change this, adjust the docs in the config.
 const DEFAULT_MULTIPART_MAX_CONCURRENT_UPLOADS: usize = 10;
 
-pub struct ConnectionWithPermit<T: Connection + AsyncRead + AsyncWrite + Unpin> {
-    connection: T,
-    _permit: SemaphorePermit<'static>,
-}
-
-impl<T: Connection + AsyncRead + AsyncWrite + Unpin> Connection for ConnectionWithPermit<T> {
-    fn connected(&self) -> Connected {
-        self.connection.connected()
-    }
-}
-
-impl<T: Connection + AsyncRead + AsyncWrite + Unpin> AsyncRead for ConnectionWithPermit<T> {
-    #[inline]
-    fn poll_read(
-        self: Pin<&mut Self>,
-        cx: &mut Context,
-        buf: &mut ReadBuf<'_>,
-    ) -> Poll<Result<(), tokio::io::Error>> {
-        Pin::new(&mut Pin::get_mut(self).connection).poll_read(cx, buf)
-    }
-}
-
-impl<T: Connection + AsyncWrite + AsyncRead + Unpin> AsyncWrite for ConnectionWithPermit<T> {
-    #[inline]
-    fn poll_write(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &[u8],
-    ) -> Poll<Result<usize, tokio::io::Error>> {
-        Pin::new(&mut Pin::get_mut(self).connection).poll_write(cx, buf)
-    }
-
-    #[inline]
-    fn poll_flush(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<Result<(), tokio::io::Error>> {
-        Pin::new(&mut Pin::get_mut(self).connection).poll_flush(cx)
-    }
-
-    #[inline]
-    fn poll_shutdown(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<Result<(), tokio::io::Error>> {
-        Pin::new(&mut Pin::get_mut(self).connection).poll_shutdown(cx)
-    }
-}
-
 #[derive(Clone)]
-pub struct TlsConnector {
-    connector: HttpsConnector<HttpConnector>,
+pub struct TlsClient {
+    client: LegacyClient<HttpsConnector<LegacyHttpConnector>, SdkBody>,
     retrier: Retrier,
 }
 
-impl TlsConnector {
+impl TlsClient {
     #[must_use]
     pub fn new(spec: &S3Spec, jitter_fn: Arc<dyn Fn(Duration) -> Duration + Send + Sync>) -> Self {
-        let connector_with_roots = hyper_rustls::HttpsConnectorBuilder::new().with_webpki_roots();
+        let connector_with_roots = HttpsConnectorBuilder::new().with_webpki_roots();
 
         let connector_with_schemes = if spec.insecure_allow_http {
             connector_with_roots.https_or_http()
@@ -157,8 +117,10 @@ impl TlsConnector {
             connector_with_schemes.enable_http1().enable_http2().build()
         };
 
+        let client = LegacyClient::builder(TokioExecutor::new()).build(connector);
+
         Self {
-            connector,
+            client,
             retrier: Retrier::new(
                 Arc::new(|duration| Box::pin(sleep(duration))),
                 jitter_fn,
@@ -166,49 +128,264 @@ impl TlsConnector {
             ),
         }
     }
+}
 
-    async fn call_with_retry(
-        &self,
-        req: &Uri,
-    ) -> Result<ConnectionWithPermit<MaybeHttpsStream<TcpStream>>, Error> {
-        let retry_stream_fn = unfold(self.connector.clone(), move |mut connector| async move {
-            let _permit = fs::get_permit().await.unwrap();
-            match connector.call(req.clone()).await {
-                Ok(connection) => Some((
-                    RetryResult::Ok(ConnectionWithPermit {
-                        connection,
-                        _permit,
-                    }),
-                    connector,
-                )),
-                Err(e) => Some((
-                    RetryResult::Retry(make_err!(
-                        Code::Unavailable,
-                        "Failed to call S3 connector: {e:?}"
-                    )),
-                    connector,
-                )),
-            }
-        });
-        self.retrier.retry(retry_stream_fn).await
+impl std::fmt::Debug for TlsClient {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        f.debug_struct("TlsClient").finish_non_exhaustive()
     }
 }
 
-impl Service<Uri> for TlsConnector {
-    type Response = ConnectionWithPermit<MaybeHttpsStream<TcpStream>>;
-    type Error = Error;
-    type Future =
-        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>>;
+impl SmithyHttpClient for TlsClient {
+    fn http_connector(
+        &self,
+        _settings: &HttpConnectorSettings,
+        _components: &RuntimeComponents,
+    ) -> SharedHttpConnector {
+        SharedHttpConnector::new(self.clone())
+    }
+}
 
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.connector
-            .poll_ready(cx)
-            .map_err(|e| make_err!(Code::Unavailable, "Failed poll in S3: {e}"))
+enum BufferedBodyState {
+    Cloneable(SdkBody),
+    Buffered(Bytes),
+    Empty,
+}
+
+mod body_processing {
+    use super::{BodyExt, BufferedBodyState, BytesMut, ConnectorError, SdkBody, TransientError};
+
+    /// Buffer a request body fully into memory.
+    ///
+    /// TODO(aaronmondal): This could lead to OOMs in extremely constrained
+    ///                    environments. Probably better to implement something
+    ///                    like a rewindable stream logic.
+    #[inline]
+    pub(crate) async fn buffer_body(body: SdkBody) -> Result<BufferedBodyState, ConnectorError> {
+        let mut bytes = BytesMut::new();
+        let mut body_stream = body;
+        while let Some(frame) = body_stream.frame().await {
+            match frame {
+                Ok(frame) => {
+                    if let Some(data) = frame.data_ref() {
+                        bytes.extend_from_slice(data);
+                    }
+                }
+                Err(e) => {
+                    return Err(ConnectorError::other(
+                        format!("Failed to read request body: {e}").into(),
+                        Some(TransientError),
+                    ));
+                }
+            }
+        }
+
+        Ok(BufferedBodyState::Buffered(bytes.freeze()))
+    }
+}
+
+struct RequestComponents {
+    method: Method,
+    uri: hyper::Uri,
+    version: hyper::Version,
+    headers: hyper::HeaderMap,
+    body_data: BufferedBodyState,
+}
+
+mod conversions {
+    use super::{
+        body_processing, BufferedBodyState, ConnectorError, Future, HttpRequest, Method,
+        RequestComponents, Response, SdkBody, TransientError,
+    };
+
+    pub(crate) trait RequestExt {
+        fn into_components(self)
+            -> impl Future<Output = Result<RequestComponents, ConnectorError>>;
     }
 
-    fn call(&mut self, req: Uri) -> Self::Future {
-        let connector_clone = self.clone();
-        Box::pin(async move { connector_clone.call_with_retry(&req).await })
+    impl RequestExt for HttpRequest {
+        async fn into_components(self) -> Result<RequestComponents, ConnectorError> {
+            // Note: This does *not* refer the the HTTP protocol, but to the
+            //       version of the http crate.
+            let hyper_req = self.try_into_http1x().map_err(|e| {
+                ConnectorError::other(
+                    format!("Failed to convert to HTTP request: {e}").into(),
+                    Some(TransientError),
+                )
+            })?;
+
+            let method = hyper_req.method().clone();
+            let uri = hyper_req.uri().clone();
+            let version = hyper_req.version();
+            let headers = hyper_req.headers().clone();
+
+            let body = hyper_req.into_body();
+
+            // Only buffer bodies for methods likely to have payloads.
+            let needs_buffering = matches!(method, Method::POST | Method::PUT);
+
+            // Preserve the body in case we need to retry.
+            let body_data = if needs_buffering {
+                if let Some(cloneable_body) = body.try_clone() {
+                    BufferedBodyState::Cloneable(cloneable_body)
+                } else {
+                    body_processing::buffer_body(body).await?
+                }
+            } else {
+                BufferedBodyState::Empty
+            };
+
+            Ok(RequestComponents {
+                method,
+                uri,
+                version,
+                headers,
+                body_data,
+            })
+        }
+    }
+
+    pub(crate) trait ResponseExt {
+        fn into_smithy_response(self) -> Response<SdkBody>;
+    }
+
+    impl ResponseExt for hyper::Response<hyper::body::Incoming> {
+        fn into_smithy_response(self) -> Response<SdkBody> {
+            let (parts, body) = self.into_parts();
+            let sdk_body = SdkBody::from_body_1_x(body);
+            let mut smithy_resp = Response::new(parts.status.into(), sdk_body);
+            let header_pairs: Vec<(String, String)> = parts
+                .headers
+                .iter()
+                .filter_map(|(name, value)| {
+                    value
+                        .to_str()
+                        .ok()
+                        .map(|value_str| (name.as_str().to_owned(), value_str.to_owned()))
+                })
+                .collect();
+
+            for (name, value) in header_pairs {
+                smithy_resp.headers_mut().insert(name, value);
+            }
+
+            smithy_resp
+        }
+    }
+}
+
+struct RequestBuilder<'a> {
+    components: &'a RequestComponents,
+}
+
+impl<'a> RequestBuilder<'a> {
+    #[inline]
+    const fn new(components: &'a RequestComponents) -> Self {
+        Self { components }
+    }
+
+    #[inline]
+    fn build(&self) -> Result<Request<SdkBody>, hyper::http::Error> {
+        let mut req_builder = Request::builder()
+            .method(self.components.method.clone())
+            .uri(self.components.uri.clone())
+            .version(self.components.version);
+
+        let headers_map = req_builder.headers_mut().unwrap();
+        for (name, value) in &self.components.headers {
+            headers_map.insert(name, value.clone());
+        }
+
+        match &self.components.body_data {
+            BufferedBodyState::Cloneable(ref body) => {
+                let cloned_body = body.try_clone().expect("Body should be cloneable");
+                req_builder.body(cloned_body)
+            }
+            BufferedBodyState::Buffered(ref bytes) => {
+                req_builder.body(SdkBody::from(bytes.clone()))
+            }
+            BufferedBodyState::Empty => req_builder.body(SdkBody::empty()),
+        }
+    }
+}
+
+mod execution {
+    use super::conversions::ResponseExt;
+    use super::{
+        fs, make_err, Code, HttpsConnector, LegacyClient, LegacyHttpConnector, RequestBuilder,
+        RequestComponents, Response, RetryResult, SdkBody,
+    };
+
+    #[inline]
+    pub(crate) async fn execute_request(
+        client: LegacyClient<HttpsConnector<LegacyHttpConnector>, SdkBody>,
+        components: &RequestComponents,
+    ) -> RetryResult<Response<SdkBody>> {
+        let _permit = match fs::get_permit().await {
+            Ok(permit) => permit,
+            Err(e) => {
+                return RetryResult::Retry(make_err!(
+                    Code::Unavailable,
+                    "Failed to acquire permit: {e}"
+                ));
+            }
+        };
+
+        let request = match RequestBuilder::new(components).build() {
+            Ok(req) => req,
+            Err(e) => {
+                return RetryResult::Err(make_err!(
+                    Code::Internal,
+                    "Failed to create request: {e}",
+                ));
+            }
+        };
+
+        match client.request(request).await {
+            Ok(resp) => RetryResult::Ok(resp.into_smithy_response()),
+            Err(e) => RetryResult::Retry(make_err!(
+                Code::Unavailable,
+                "Failed request in S3Store: {e}"
+            )),
+        }
+    }
+
+    #[inline]
+    pub(crate) fn create_retry_stream(
+        client: LegacyClient<HttpsConnector<LegacyHttpConnector>, SdkBody>,
+        components: RequestComponents,
+    ) -> impl futures::Stream<Item = RetryResult<Response<SdkBody>>> {
+        futures::stream::unfold(components, move |components| {
+            let client_clone = client.clone();
+            async move {
+                let result = execute_request(client_clone, &components).await;
+
+                Some((result, components))
+            }
+        })
+    }
+}
+
+impl SmithyHttpConnector for TlsClient {
+    fn call(&self, req: HttpRequest) -> HttpConnectorFuture {
+        use conversions::RequestExt;
+
+        let client = self.client.clone();
+        let retrier = self.retrier.clone();
+
+        HttpConnectorFuture::new(Box::pin(async move {
+            let components = req.into_components().await?;
+
+            let retry_stream = execution::create_retry_stream(client, components);
+
+            match retrier.retry(retry_stream).await {
+                Ok(response) => Ok(response),
+                Err(e) => Err(ConnectorError::other(
+                    format!("Connection failed after retries: {e}").into(),
+                    Some(TransientError),
+                )),
+            }
+        }))
     }
 }
 
@@ -269,14 +446,18 @@ where
             delay.mul_f32(rand::rng().random_range(min..max))
         });
         let s3_client = {
-            let http_client =
-                HyperClientBuilder::new().build(TlsConnector::new(spec, jitter_fn.clone()));
-            let credential_provider = credentials::default_provider().await;
-            let mut config_builder = aws_config::defaults(BehaviorVersion::v2024_03_28())
-                // TODO(aaronmondal): Flip these to the default "WhenSupported".
-                //                    See: https://github.com/awslabs/aws-sdk-rust/releases/tag/release-2025-01-15
-                .request_checksum_calculation(RequestChecksumCalculation::WhenRequired)
-                .response_checksum_validation(ResponseChecksumValidation::WhenRequired)
+            let http_client = TlsClient::new(&spec.clone(), jitter_fn.clone());
+
+            let credential_provider = credentials::DefaultCredentialsChain::builder()
+                .configure(
+                    ProviderConfig::without_region()
+                        .with_region(Some(Region::new(Cow::Owned(spec.region.clone()))))
+                        .with_http_client(http_client.clone()),
+                )
+                .build()
+                .await;
+
+            let config = aws_config::defaults(BehaviorVersion::v2025_01_17())
                 .credentials_provider(credential_provider)
                 .app_name(AppName::new("nativelink").expect("valid app name"))
                 .timeout_config(
@@ -285,14 +466,11 @@ where
                         .build(),
                 )
                 .region(Region::new(Cow::Owned(spec.region.clone())))
-                .http_client(http_client);
-            // TODO(allada) When aws-sdk supports this env variable we should be able
-            // to remove this.
-            // See: https://github.com/awslabs/aws-sdk-rust/issues/932
-            if let Ok(endpoint_url) = env::var("AWS_ENDPOINT_URL") {
-                config_builder = config_builder.endpoint_url(endpoint_url);
-            }
-            aws_sdk_s3::Client::new(&config_builder.load().await)
+                .http_client(http_client)
+                .load()
+                .await;
+
+            aws_sdk_s3::Client::new(&config)
         };
         Self::new_with_client_and_jitter(spec, s3_client, jitter_fn, now_fn)
     }
