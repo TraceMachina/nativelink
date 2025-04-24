574f4d72840efbf3cc759979767a8802964f5ae1
rafaelre898 <rafaelre898@gmail.com>
2025-04-25 01:32:23 +0500

    Added timestamp to BEP redis key behind feature flag #1251

--- nativelink-config/src/stores.rs
+++ nativelink-config/src/stores.rs
@@ -976,6 +976,9 @@ pub struct RedisSpec {
     #[serde(default, deserialize_with = "convert_numeric_with_shellexpand")]
     pub max_chunk_uploads_per_update: usize,
 
+    #[serde(default)]
+    pub experimental_pub_sub_timestamp: Option<bool>,
+
     /// Retry configuration to use when a network request fails.
     /// See the `Retry` struct for more information.
     ///
