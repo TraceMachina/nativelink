c7231ff4e420ac806be3c4f7f2120b29c3af57e1
Evaan Yaser Ahmed <ahmedeva@grinnell.edu>
2025-05-20 17:53:37 -0500

    Add a Contributors Section To Website's Home Page

--- web/platform/src/components/qwik/pages/home.tsx
+++ web/platform/src/components/qwik/pages/home.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 import { Benefits } from "../sections/benefits.tsx";
 import { Community } from "../sections/community.tsx";
-import { Engineers } from "../sections/engineers.tsx";
+import { Contributors } from "../sections/contributors.tsx";
 import { Features } from "../sections/feature.tsx";
 import { Hero } from "../sections/hero.tsx";
 import { Testimonial } from "../sections/testimonials.tsx";
@@ -11,9 +11,9 @@ export const LandingPage = component$(() => {
   return (
     <main class="w-full z-20 bg-black font-nunito text-white">
       <Hero />
+      <Contributors />
       <Testimonial />
       <Features />
-      <Engineers />
       <Benefits />
       <Community />
     </main>
