c45b0a430734897b8f5224ddee038541489ba180
Evaan Yaser Ahmed <ahmedeva@grinnell.edu>
2025-05-20 17:53:37 -0500

    Add a Clients Section To Website's Home Page

--- web/platform/src/components/qwik/pages/home.tsx
+++ web/platform/src/components/qwik/pages/home.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 import { Benefits } from "../sections/benefits.tsx";
 import { Community } from "../sections/community.tsx";
-import { Engineers } from "../sections/engineers.tsx";
+import { Customers } from "../sections/customers.tsx";
 import { Features } from "../sections/feature.tsx";
 import { Hero } from "../sections/hero.tsx";
 import { Testimonial } from "../sections/testimonials.tsx";
@@ -11,9 +11,9 @@ export const LandingPage = component$(() => {
   return (
     <main class="w-full z-20 bg-black font-nunito text-white">
       <Hero />
+      <Customers />
       <Testimonial />
       <Features />
-      <Engineers />
       <Benefits />
       <Community />
     </main>
---
+++ web/platform/src/components/qwik/sections/customers.tsx
@@ -0,0 +1,79 @@
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+
+import { Label } from "../components/text.tsx";
+
+import {
+  Citrix,
+  MenloSecurity,
+  Meta,
+  Neuralink,
+  Samsung,
+  Tesla,
+  ThirdWave,
+} from "../../media/icons/customers.tsx";
+
+export const Customers = component$(() => {
+  const isMobile = useSignal(false);
+
+  useVisibleTask$(() => {
+    isMobile.value = window.innerWidth <= 768;
+
+    const handleResize = () => {
+      isMobile.value = window.innerWidth <= 768;
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
+  const logos_row_width = isMobile.value ? "" : "w-[70vw]"; // Use default width on mobile
+  const logo_width = isMobile.value ? "25vw" : "15vw";
+  const logos_top_margin = isMobile.value ? "mt-32" : "mt-16";
+  const logos_bottom_margin = isMobile.value ? "-mb-6" : "mb-8";
+
+  const companies = [
+    {
+      img: <Neuralink width={logo_width} />,
+    },
+    {
+      img: <MenloSecurity width={logo_width} />,
+    },
+    {
+      img: <Citrix width={logo_width} />,
+    },
+    {
+      img: <Tesla width={logo_width} />,
+    },
+    {
+      img: <Meta width={logo_width} />,
+    },
+    {
+      img: <Samsung width={logo_width} />,
+    },
+    {
+      img: <ThirdWave width={logo_width} />,
+    },
+  ];
+
+  return (
+    <div class="flex min-h-fit w-full flex-col items-center justify-center py-16">
+      {/* Row 1 - Label */}
+      <div class={"flex items-center justify-center"}>
+        <Label
+          text="Some companies we are building with"
+          class={"text-base px-4"}
+        />
+      </div>
+
+      {/* Row 2 - Empty spacer */}
+      <div class="flex h-[5vh] w-full"> </div>
+
+      {/* Row 3 - Display logos */}
+      <div
+        class={`flex min-h-fit ${logos_row_width} items-center justify-evenly gap-6 flex-wrap ${logos_top_margin} ${logos_bottom_margin}`}
+      >
+        {companies.map((company) => company.img)}
+      </div>
+    </div>
+  );
+});
