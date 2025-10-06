589b190601e9f50e168d0a1cd0235be3a4d10de6
Marcus <marcuseagan@gmail.com>
2025-10-06 00:21:21 -0700

    include header

--- deployment-examples/juicefs-native-setup.sh
+++ deployment-examples/juicefs-native-setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2024 The NativeLink Authors. All rights reserved.
+#
+# Licensed under the Functional Source License, Version 1.1, Apache 2.0 Future License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    See LICENSE file for details
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Native JuiceFS Setup for NativeLink Multi-Worker
 # This script sets up JuiceFS WITHOUT Docker
 
