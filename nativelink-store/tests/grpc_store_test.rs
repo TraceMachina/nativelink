ff15e88fa210c97049ea61d853437120cae056e3
Tom Parker-Shemilt <tom@tracemachina.com>
2026-04-28 15:27:16 +0100

    Add grpc test with an actual gRPC server (#2287)

--- nativelink-store/tests/grpc_store_test.rs
+++ nativelink-store/tests/grpc_store_test.rs
@@ -1,21 +1,39 @@
+use core::pin::Pin;
 use core::time::Duration;
+use std::sync::Arc;
 
+use async_lock::Mutex;
+use futures::{Stream, StreamExt};
 use nativelink_config::stores::{GrpcEndpoint, GrpcSpec, Retry, StoreType};
-use nativelink_error::Error;
+use nativelink_error::{Error, ResultExt};
 use nativelink_macro::nativelink_test;
 use nativelink_proto::build::bazel::remote::execution::v2::{
     FindMissingBlobsRequest, digest_function,
 };
+use nativelink_proto::google::bytestream::byte_stream_server::{ByteStream, ByteStreamServer};
+use nativelink_proto::google::bytestream::{
+    QueryWriteStatusRequest, QueryWriteStatusResponse, ReadRequest, ReadResponse, WriteRequest,
+    WriteResponse,
+};
 use nativelink_store::grpc_store::GrpcStore;
+use nativelink_util::background_spawn;
+use nativelink_util::buf_channel::make_buf_channel_pair;
+use nativelink_util::common::DigestInfo;
+use nativelink_util::store_trait::{StoreLike, UploadSizeInfo};
+use regex::Regex;
 use tokio::time::timeout;
-use tonic::Request;
+use tonic::transport::Server;
+use tonic::transport::server::TcpIncoming;
+use tonic::{Request, Response, Status, Streaming};
+use tracing::info;
 
-#[nativelink_test]
-async fn fast_find_missing_blobs() -> Result<(), Error> {
-    let spec = GrpcSpec {
+const VALID_HASH: &str = "0123456789abcdef000000000000000000010000000000000123456789abcdef";
+
+fn test_spec<T: Into<String>>(endpoint: T) -> GrpcSpec {
+    GrpcSpec {
         instance_name: String::new(),
         endpoints: vec![GrpcEndpoint {
-            address: "http://foobar".into(),
+            address: endpoint.into(),
             tls_config: None,
             concurrency_limit: None,
             connect_timeout_s: 0,
@@ -28,7 +46,12 @@ async fn fast_find_missing_blobs() -> Result<(), Error> {
         max_concurrent_requests: 0,
         connections_per_endpoint: 0,
         rpc_timeout_s: 1,
-    };
+    }
+}
+
+#[nativelink_test]
+async fn fast_find_missing_blobs() -> Result<(), Error> {
+    let spec = test_spec("http://foobar");
     let store = GrpcStore::new(&spec).await?;
     let request = Request::new(FindMissingBlobsRequest {
         instance_name: String::new(),
@@ -41,5 +64,112 @@ async fn fast_find_missing_blobs() -> Result<(), Error> {
     .await??;
     let inner_res = res.into_inner();
     assert_eq!(inner_res.missing_blob_digests.len(), 0);
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+struct FakeStreamServer {
+    write_requests: Arc<Mutex<Vec<WriteRequest>>>,
+}
+
+impl FakeStreamServer {
+    fn new() -> Self {
+        Self {
+            write_requests: Arc::new(Mutex::new(vec![])),
+        }
+    }
+}
+
+type ReadStream = Pin<Box<dyn Stream<Item = Result<ReadResponse, Status>> + Send + 'static>>;
+
+#[tonic::async_trait]
+impl ByteStream for FakeStreamServer {
+    type ReadStream = ReadStream;
+
+    #[allow(clippy::unimplemented)]
+    async fn read(
+        &self,
+        _grpc_request: Request<ReadRequest>,
+    ) -> Result<Response<Self::ReadStream>, Status> {
+        unimplemented!();
+    }
+
+    async fn write(
+        &self,
+        grpc_request: Request<Streaming<WriteRequest>>,
+    ) -> Result<Response<WriteResponse>, Status> {
+        let write_request = match grpc_request.into_inner().next().await {
+            None => {
+                return Err(Status::unknown("Client closed stream"));
+            }
+            Some(Err(err)) => return Err(err),
+            Some(Ok(write_request)) => write_request,
+        };
+        info!(?write_request, "write request");
+        let committed_size = write_request.data.len() as i64;
+        self.write_requests.lock().await.push(write_request);
+        Ok(Response::new(WriteResponse { committed_size }))
+    }
+
+    #[allow(clippy::unimplemented)]
+    async fn query_write_status(
+        &self,
+        _grpc_request: Request<QueryWriteStatusRequest>,
+    ) -> Result<Response<QueryWriteStatusResponse>, Status> {
+        unimplemented!();
+    }
+}
+
+async fn make_fake_bytestream_server() -> (FakeStreamServer, u16) {
+    let fake_stream_server = FakeStreamServer::new();
+    let server = ByteStreamServer::new(fake_stream_server.clone());
+    let listener = TcpIncoming::bind("127.0.0.1:0".parse().unwrap()).unwrap();
+    let port = listener.local_addr().unwrap().port();
+
+    background_spawn!("server", async move {
+        Server::builder()
+            .add_service(server)
+            .serve_with_incoming(listener)
+            .await
+            .unwrap();
+    });
+
+    (fake_stream_server, port)
+}
+
+#[nativelink_test]
+async fn write_update_works() -> Result<(), Error> {
+    const RAW_INPUT: &str = "123";
+
+    let (server, port) = make_fake_bytestream_server().await;
+    let spec = test_spec(format!("http://localhost:{port}"));
+    let store = GrpcStore::new(&spec).await?;
+    let digest = DigestInfo::try_new(VALID_HASH, RAW_INPUT.len()).unwrap();
+
+    let (mut tx, rx) = make_buf_channel_pair();
+    let send_fut = async move {
+        tx.send(RAW_INPUT.into()).await?;
+        tx.send_eof()
+    };
+    let (res1, res2) = futures::join!(
+        send_fut,
+        store.update(
+            digest,
+            rx,
+            UploadSizeInfo::ExactSize(RAW_INPUT.len().try_into().unwrap())
+        )
+    );
+    res1.merge(res2)?;
+
+    let write_requests = server.write_requests.lock().await;
+    assert_eq!(write_requests.len(), 1);
+    let write_request = write_requests.first().unwrap();
+    let upload_pattern = Regex::new("/uploads/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/blobs/sha256/0123456789abcdef000000000000000000010000000000000123456789abcdef/3").unwrap();
+    assert!(
+        upload_pattern.is_match(&write_request.resource_name),
+        "resource name: {}",
+        write_request.resource_name
+    );
+    assert_eq!(write_request.data, RAW_INPUT.as_bytes());
     Ok(())
 }
