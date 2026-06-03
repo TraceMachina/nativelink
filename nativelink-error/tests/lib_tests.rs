ce957a4d992792fcf978e194ad7486b11ffd77fe
Tom Parker-Shemilt <tom@tracemachina.com>
2026-06-03 15:37:51 +0100

    Bring tests up-to-date with linting standards

--- nativelink-error/tests/lib_tests.rs
+++ nativelink-error/tests/lib_tests.rs
@@ -12,40 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use core::num::{ParseIntError, TryFromIntError};
 use std::borrow::Cow;
 use std::io;
-use std::num::{ParseIntError, TryFromIntError};
 
-use fred::error::{RedisError, RedisErrorKind};
-use hex::FromHexError;
-use nativelink_error::{make_err, make_input_err, Code, Error, ResultExt};
+use nativelink_error::{Code, Error, ErrorContext, ResultExt, make_err, make_input_err};
 use nativelink_metric::{MetricFieldData, MetricKind, MetricsComponent};
 use nativelink_proto::google::rpc::Status;
-use serde::de::Error as DeError;
-use serde::ser::Error as SerError;
+use redis::ErrorKind as RedisErrorKind;
 use tokio::runtime::Runtime;
 use tokio::task;
 use tonic::{Code as TonicCode, Status as TonicStatus};
 
-#[test]
-fn test_serde_serialization_error_custom() {
-    let error = <Error as SerError>::custom("Serialization failed due to invalid input");
-    assert_eq!(error.code, Code::InvalidArgument);
-    assert_eq!(
-        error.messages,
-        vec!["Serialization failed due to invalid input".to_string()]
-    );
-}
-
-#[test]
-fn test_serde_deserialization_error_custom() {
-    let error = <Error as DeError>::custom("Deserialization failed due to corrupted data");
-    assert_eq!(error.code, Code::InvalidArgument);
-    assert_eq!(
-        error.messages,
-        vec!["Deserialization failed due to corrupted data".to_string()]
-    );
-}
 #[test]
 fn test_err_tip_with_code_some() {
     let option = Some(42);
@@ -65,28 +43,14 @@ fn test_err_tip_with_code_none() {
     assert_eq!(error.code, Code::InvalidArgument);
     assert_eq!(error.messages, vec!["Missing value in option".to_string()]);
 }
-#[test]
-fn test_code_to_error_conversion() {
-    let error: Error = Code::InvalidArgument.into();
-    println!(
-        "Debug: Error Code - {:?}, Messages - {:?}",
-        error.code, error.messages
-    );
-    assert_eq!(
-        error.code,
-        Code::InvalidArgument,
-        "Expected code to be InvalidArgument"
-    );
-    assert!(
-        error.messages.is_empty() || error.messages[0].is_empty(),
-        "Expected messages to be empty or contain an empty string, but found: {:?}",
-        error.messages
-    );
-}
 
 #[test]
 fn test_redis_error_conversion_invalid_argument() {
-    let redis_error = RedisError::new(RedisErrorKind::InvalidArgument, "Invalid argument error");
+    let redis_error: redis::RedisError = (
+        RedisErrorKind::InvalidClientConfig,
+        "Invalid argument error",
+    )
+        .into();
     let error: Error = redis_error.into();
     assert_eq!(error.code, Code::InvalidArgument);
     assert!(error.messages[0].contains("Invalid argument error"));
@@ -94,7 +58,7 @@ fn test_redis_error_conversion_invalid_argument() {
 
 #[test]
 fn test_redis_error_conversion_internal() {
-    let redis_error = RedisError::new(RedisErrorKind::IO, "Internal error");
+    let redis_error: redis::RedisError = (RedisErrorKind::Io, "Internal error").into();
     let error: Error = redis_error.into();
     assert_eq!(error.code, Code::Internal);
     assert!(error.messages[0].contains("Internal error"));
@@ -102,18 +66,14 @@ fn test_redis_error_conversion_internal() {
 
 #[test]
 fn test_redis_error_conversion_permission_denied() {
-    let redis_error = RedisError::new(RedisErrorKind::Auth, "Permission denied error");
+    let redis_error: redis::RedisError = (
+        RedisErrorKind::AuthenticationFailed,
+        "Permission denied error",
+    )
+        .into();
     let error: Error = redis_error.into();
     assert_eq!(error.code, Code::PermissionDenied);
     assert!(error.messages[0].contains("Permission denied error"));
-}
-
-#[test]
-fn test_redis_error_conversion_timeout() {
-    let redis_error = RedisError::new(RedisErrorKind::Timeout, "Timeout error");
-    let error: Error = redis_error.into();
-    assert_eq!(error.code, Code::DeadlineExceeded);
-    assert!(error.messages[0].contains("Timeout error"));
 }
 
 #[test]
@@ -143,12 +103,15 @@ fn test_error_to_status_conversion() {
     let error = Error {
         code: Code::Internal,
         messages: vec!["An internal error occurred".to_string()],
+        context: ErrorContext::None,
     };
     let tonic_status: TonicStatus = error.into();
     assert_eq!(tonic_status.code(), TonicCode::Internal);
-    assert!(tonic_status
-        .message()
-        .contains("An internal error occurred"));
+    assert!(
+        tonic_status
+            .message()
+            .contains("An internal error occurred")
+    );
 }
 
 #[test]
@@ -175,14 +138,7 @@ fn test_parse_int_error_conversion() {
 }
 
 #[test]
-fn test_from_hex_error_conversion() {
-    let hex_error = FromHexError::InvalidStringLength;
-    let error: Error = hex_error.into();
-    assert_eq!(error.code, Code::InvalidArgument);
-    assert!(error.messages[0].contains("Invalid string length"));
-}
-
-#[test]
+#[expect(clippy::disallowed_methods, reason = "testing implementation")]
 fn test_join_error_conversion() {
     let runtime = Runtime::new().expect("Failed to create Tokio runtime");
 
@@ -245,9 +201,9 @@ fn test_metrics_component_publish() {
     let result = error.publish(kind, field_metadata);
     match result {
         Ok(data) => {
-            println!("Published data: {:?}", data);
+            println!("Published data: {data:?}");
         }
-        Err(e) => panic!("Publish failed with error: {:?}", e),
+        Err(e) => panic!("Publish failed with error: {e:?}"),
     }
 }
 
@@ -302,9 +258,11 @@ fn test_error_message_string() {
 fn test_error_make_err_macro() {
     let error = make_err!(Code::InvalidArgument, "Invalid argument: {}", "value");
     assert_eq!(error.code, Code::InvalidArgument);
-    assert!(error
-        .messages
-        .contains(&"Invalid argument: value".to_string()));
+    assert!(
+        error
+            .messages
+            .contains(&"Invalid argument: value".to_string())
+    );
 }
 
 #[test]
@@ -349,90 +307,6 @@ fn test_result_ext_merge() {
     let merged_result = result1.merge(result2);
     let error = merged_result.unwrap_err();
     assert_eq!(error.messages, vec!["First error", "---", "Second error"]);
-}
-
-#[test]
-fn test_tonic_code_to_code_conversion() {
-    assert_eq!(Code::from(TonicCode::Ok), Code::Ok);
-    assert_eq!(Code::from(TonicCode::Cancelled), Code::Cancelled);
-    assert_eq!(Code::from(TonicCode::Unknown), Code::Unknown);
-    assert_eq!(
-        Code::from(TonicCode::InvalidArgument),
-        Code::InvalidArgument
-    );
-    assert_eq!(
-        Code::from(TonicCode::DeadlineExceeded),
-        Code::DeadlineExceeded
-    );
-    assert_eq!(Code::from(TonicCode::NotFound), Code::NotFound);
-    assert_eq!(Code::from(TonicCode::AlreadyExists), Code::AlreadyExists);
-    assert_eq!(
-        Code::from(TonicCode::PermissionDenied),
-        Code::PermissionDenied
-    );
-    assert_eq!(
-        Code::from(TonicCode::ResourceExhausted),
-        Code::ResourceExhausted
-    );
-    assert_eq!(
-        Code::from(TonicCode::FailedPrecondition),
-        Code::FailedPrecondition
-    );
-    assert_eq!(Code::from(TonicCode::Aborted), Code::Aborted);
-    assert_eq!(Code::from(TonicCode::OutOfRange), Code::OutOfRange);
-    assert_eq!(Code::from(TonicCode::Unimplemented), Code::Unimplemented);
-    assert_eq!(Code::from(TonicCode::Internal), Code::Internal);
-    assert_eq!(Code::from(TonicCode::Unavailable), Code::Unavailable);
-    assert_eq!(Code::from(TonicCode::DataLoss), Code::DataLoss);
-    assert_eq!(
-        Code::from(TonicCode::Unauthenticated),
-        Code::Unauthenticated
-    );
-}
-
-#[test]
-fn test_code_to_tonic_code_conversion() {
-    assert_eq!(TonicCode::from(Code::Ok), TonicCode::Ok);
-    assert_eq!(TonicCode::from(Code::Cancelled), TonicCode::Cancelled);
-    assert_eq!(TonicCode::from(Code::Unknown), TonicCode::Unknown);
-    assert_eq!(
-        TonicCode::from(Code::InvalidArgument),
-        TonicCode::InvalidArgument
-    );
-    assert_eq!(
-        TonicCode::from(Code::DeadlineExceeded),
-        TonicCode::DeadlineExceeded
-    );
-    assert_eq!(TonicCode::from(Code::NotFound), TonicCode::NotFound);
-    assert_eq!(
-        TonicCode::from(Code::AlreadyExists),
-        TonicCode::AlreadyExists
-    );
-    assert_eq!(
-        TonicCode::from(Code::PermissionDenied),
-        TonicCode::PermissionDenied
-    );
-    assert_eq!(
-        TonicCode::from(Code::ResourceExhausted),
-        TonicCode::ResourceExhausted
-    );
-    assert_eq!(
-        TonicCode::from(Code::FailedPrecondition),
-        TonicCode::FailedPrecondition
-    );
-    assert_eq!(TonicCode::from(Code::Aborted), TonicCode::Aborted);
-    assert_eq!(TonicCode::from(Code::OutOfRange), TonicCode::OutOfRange);
-    assert_eq!(
-        TonicCode::from(Code::Unimplemented),
-        TonicCode::Unimplemented
-    );
-    assert_eq!(TonicCode::from(Code::Internal), TonicCode::Internal);
-    assert_eq!(TonicCode::from(Code::Unavailable), TonicCode::Unavailable);
-    assert_eq!(TonicCode::from(Code::DataLoss), TonicCode::DataLoss);
-    assert_eq!(
-        TonicCode::from(Code::Unauthenticated),
-        TonicCode::Unauthenticated
-    );
 }
 
 #[test]
