9a5843b61cf8531d84432a4a12828b9b5744d3be
DEBANJAN MUKHERJEE <113274631+RickDeb2004@users.noreply.github.com>
2026-06-11 17:53:42 +0530

    Prevent CAS and AC from using the same store (#1177)
    * Fix:Prevent CAS and AC from using the same store
    * Pull config duplication check into nativelink-config and test
    
    ---------
    
    Co-authored-by: Tom Parker-Shemilt <tom@tracemachina.com>

--- flake.nix
+++ flake.nix
@@ -98,7 +98,7 @@
         src = pkgs.lib.cleanSourceWith {
           src = (craneLibFor pkgs).path ./.;
           filter = path: type:
-            (builtins.match "^.*(examples/.+\.json5|data/.+|nativelink-config/README\.md)" path != null)
+            (builtins.match "^.*(tests/.+\.json5|examples/.+\.json5|data/.+|nativelink-config/README\.md)" path != null)
             || ((craneLibFor pkgs).filterCargoSources path type);
         };
 
