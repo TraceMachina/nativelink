2978a1dbd496971e7ca77198b8de53ff26164e21
Aman Kumar <aman@tracemachina.com>
2026-08-22 04:21:27 +0530

    Match undeclared platform properties dynamically instead of rejecting them (#2705)

--- nativelink-util/tests/platform_properties_tests.rs
+++ nativelink-util/tests/platform_properties_tests.rs
@@ -21,6 +21,43 @@ fn ignore_property_match_all() {
     assert!(ignore_properties.is_satisfied_by(&PlatformProperties::new(HashMap::new()), true));
 }
 
+#[test]
+fn unknown_property_matches_worker_with_same_value() {
+    let unknown_property = PlatformPropertyValue::Unknown("foo".to_string());
+    assert!(unknown_property.is_satisfied_by(&PlatformPropertyValue::Unknown("foo".to_string())));
+    assert!(unknown_property.is_satisfied_by(&PlatformPropertyValue::Exact("foo".to_string())));
+    assert!(!unknown_property.is_satisfied_by(&PlatformPropertyValue::Unknown("bar".to_string())));
+    assert!(!unknown_property.is_satisfied_by(&PlatformPropertyValue::Exact("bar".to_string())));
+}
+
+#[test]
+fn unknown_property_does_not_restrict_worker_without_key() {
+    let mut property_map = HashMap::new();
+    property_map.insert(
+        "foo".into(),
+        PlatformPropertyValue::Unknown("bar".to_string()),
+    );
+    let unknown_properties = PlatformProperties::new(property_map);
+
+    // A worker that does not declare the key is not restricted by it.
+    assert!(unknown_properties.is_satisfied_by(&PlatformProperties::new(HashMap::new()), true));
+
+    // A worker that declares the key must match the value.
+    let mut mismatched_map = HashMap::new();
+    mismatched_map.insert(
+        "foo".into(),
+        PlatformPropertyValue::Unknown("baz".to_string()),
+    );
+    assert!(!unknown_properties.is_satisfied_by(&PlatformProperties::new(mismatched_map), true));
+
+    let mut matched_map = HashMap::new();
+    matched_map.insert(
+        "foo".into(),
+        PlatformPropertyValue::Unknown("bar".to_string()),
+    );
+    assert!(unknown_properties.is_satisfied_by(&PlatformProperties::new(matched_map), true));
+}
+
 #[nativelink_test]
 fn minimum_property_logs_error() {
     let minimum_property = PlatformPropertyValue::Minimum(1);
